a8b8ee6d7280ae63a56349557be9d47ee887627e
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 16:41:32 +0200

    Create de-DE.php

---
+++ upload/converter/language/de-DE/de-DE.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version		$Id: NAME.xxx 2014-10-17 9:18Z mic $
+ * @package		_PROJECT_NAME
+ * @author		mic - http://osworx.net
+ * @copyright	2014 OSWorX - http://osworx.net
+ * @license		AGPL - www.gnu.org/licenses/agpl-3.0.html
+ */
+
+$_['code']			= 'de';
+$_['name']			= 'Deutsch (DE)';
+$_['direction']			= 'ltr';
+$_['button_save']		= 'Speichern';
+$_['text_home']                 = 'Home';
