7e76f5ad34549b73f9512e0088105b2e8aafe577
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 17:34:25 +0200

    Update setting.php

--- upload/converter/language/finnish/common/setting/setting.php
+++ upload/converter/language/finnish/common/setting/setting.php
@@ -1,10 +1,18 @@
 <?php
 // Heading
-$_['heading_title']                 = 'Aseta ylläpitäjän käyttöoikeudet';
+$_['heading_title']                = 'Asetukset';
 
 // Text
-$_['button_upgrade']                = 'Päivityksen aloitukseen';
-$_['text_permissions']              = 'Ylläpitäjän oikeudet';
-$_['text_new_permissions']          = 'Uudet käyttöoikeudet:';
+$_['text_admin_language']          = 'Vaihda hallintapaneelin kieli';
+$_['text_success']                 = 'Onnistui: Olet muuttanut asetuksia!';
 
+// Button
+$_['button_cancel']                = 'Peruuta';
+
+// Entry
+$_['entry_admin_language']         = 'Hallintapaneelin kieli:';
+
+// Error
+$_['error_warning']                = 'Varoitus: Tarkista lomakken virheet!';
+$_['error_permission']             = 'Varoitus: Sinulla ei ole oikeuksia muokata asetuksia!';
 ?>
