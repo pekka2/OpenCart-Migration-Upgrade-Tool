3d26945ff526df08e7209eb5bb0ee4a195f998b9
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 17:30:33 +0200

    Create setting.php

---
+++ upload/converter/language/finnish/common/setting/setting.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Aseta ylläpitäjän käyttöoikeudet';
+
+// Text
+$_['button_upgrade']                = 'Päivityksen aloitukseen';
+$_['text_permissions']              = 'Ylläpitäjän oikeudet';
+$_['text_new_permissions']          = 'Uudet käyttöoikeudet:';
+
+?>
