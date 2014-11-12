67afb8e4bd7efc15bbfeda513a87af7fca04fa11
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 16:57:18 +0200

    Create configuration.php

---
+++ upload/converter/language/english/upgrade/configuration.php
@@ -0,0 +1,39 @@
+<?php
+// Heading
+$_['heading_title']             = 'Edit of Configuration Files';
+
+// Text
+$_['text_curr_setting']	        = '(current setting)';
+$_['text_images_info']          = 'Upgrade of Imagepaths';
+$_['text_intro_3']		= 'This updater can be operated in a <b>simulation</b> mode also.<br />Enable this option and no real operations are made.';
+$_['text_intro_step_3']	        = 'This will rename <b>image/data</b> directories and update image paths in database';
+$_['text_off']			= 'OFF';
+$_['text_on']			= 'ON';
+$_['text_upgrade_info']         = 'Upgrade of Configuration';
+$_['text_simulation']	        = 'Simulation is';
+$_['text_step_3_3']		= 'Step 3 / 3';
+$_['text_update_config']        = 'Now you can also update the <b>config.php</b> files if not already done';
+// Entry
+$_['entry_adminDir']	        = 'Name of directory for adminfiles';
+$_['entry_imageDir']		= 'Name of  image directory';
+$_['entry_oldDir']		= 'Path of old installation';
+$_['entry_perms']		= 'Your server writing permission';
+// Btn
+$_['btn_skip']			= 'Skip';
+$_['btn_continue']		= 'Continue';
+$_['btn_cancel']		= 'Cancel';
+$_['button_database']               = 'To Upgrade Database';
+// Header
+$_['header_step_3']		= 'Adjust image paths';
+// help
+$_['help_ops']			= 'Display Operations <small>(display all database operations)</small>';
+$_['help_perms']		= '(write permission of directory <b>system/logs</b>)';
+$_['help_simulate']		= 'Simulate Converting <small>(simulating operations only)</small>';
+// msg
+$_['msg_config_added']  	= 'Setting <b>%s</b> successfully added to <b>%s</b>';
+$_['msg_config_constant']	= 'Setting <b>%s</b>  successfully added to <b>%s</b>!';
+$_['msg_config_replace']        = 'Filepath in configurationfile <b>%s</b> adjusted';
+$_['msg_config_skipped']        = 'Adopting configuration files skipped';
+$_['msg_config_uptodate']	= 'Configurationfile <b>%s</b> already up-to-date';
+$_['msg_perm_file']		= 'No permission to write file <b>%s</b>, to proceed change first filepermission to 644 or 666';
+?>
