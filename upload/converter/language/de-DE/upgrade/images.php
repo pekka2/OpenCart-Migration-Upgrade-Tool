6d2d4b7c4c05b1df570ebc489449f626b629f85d
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 16:49:54 +0200

    Create images.php

---
+++ upload/converter/language/de-DE/upgrade/images.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']             = 'Copying Image Files and Upgrade Image paths';
+
+// Text
+$_['text_error']		= 'Fehler';
+$_['text_finish']		= 'Fertig';
+$_['text_finish_text']	        = 'Glückwunsch!<br />Der alte Shop wurde erfolgreich auf die neueste Version aktualisiert. Bitte untenstehende Buttons zum Fortfahren verwenden.';
+$_['text_finish_note']   	= '<b>Hinweis</b>: nicht vergessen den Ordner <b>converter</b> zu löschen!';
+$_['text_off']			= 'AUS';
+$_['text_on']			= 'AN';
+$_['text_simulation']	        = 'Simulation ist';
+$_['text_update_config']        = 'Falls noch nicht bereits erledigt, können jetzt die <b>Konfiguationsdateien</b> aktualisiert werden';
+// msg
+$_['msg_change_path']	        = 'Erfolgreich <b>%s</b> Bildpfad(e) angepasst';
+$_['msg_copy_image']    	= 'Erfolgreich <b>%s</b> Bild(er) kopiert';
+$_['msg_errors']	= 'Directory <b>%s</b> is not found! You check path.';
+$_['msg_image_copied']	        = 'Bild <b>%s/%s</b> erfolgreich kopiert';
+$_['msg_image_path']	        = 'Bilderpfad erfolgreich geändert';
+$_['msg_image_skipped']	        = 'Anpassung der Bildpfade übersprungen';
+$_['msg_new_dir']		= '+ <b>%s</b> neue Verzeichnis(se)';
+$_['msg_newDir']		= 'Ordner <b>%s</b> erfolgreich neu erstellt';
+$_['msg_perm_dir']		= 'Keine Schreibrechte für Ordner <b>%s</b>. Um fortzufahren bitte zuerst ändern (0755 oder 0777)';
+$_['msg_renamed_dir']	        = 'Verzeichnis <b>%s</b> erfolgreich in <b>%s</b> umbenannt';
+$_['msg_renamed_total_dir']     = 'Renamed total <b>%s</b> directories successfully';
+?>
