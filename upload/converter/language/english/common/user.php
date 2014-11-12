18e63b4acbd77c52cb85e2921a250d205bb9716a
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 16:54:57 +0200

    Create user.php

---
+++ upload/converter/language/english/common/user.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Set a full administrative access';
+
+// Text
+$_['button_upgrade']                = 'Initial Upgrade';
+$_['text_permissions']              = 'Administrative access';
+$_['text_new_permissions']          = 'New permissions:';
+
+// Error
+$_['error_install']                 = 'Warning: Install folder still exists and should be deleted for security reasons!';
+$_['error_cache']                   = 'Warning: Cache directory %s not writeable!';
+$_['error_download']                = 'Warning: Download directory %s not writeable!';
+$_['error_logs']                    = 'Warning: Log directory %s not writeable!';
+?>
