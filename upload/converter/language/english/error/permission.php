c2f113cc83caefe3702028197acf4af5978e203f
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 16:55:50 +0200

    Create permission.php

---
+++ upload/converter/language/english/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Permission Denied!'; 
+
+// Text
+$_['button_permission']     = 'Administrative access';
+$_['text_permission']       = 'You do not have permission to access this page, please refer to your system administrator.';
+?>
