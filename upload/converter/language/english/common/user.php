6cd3db5907dd7cf183c99c162f6902af1710c785
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-12 20:10:09 +0200

    Update user.php

--- upload/converter/language/english/common/user.php
+++ upload/converter/language/english/common/user.php
@@ -8,8 +8,5 @@ $_['text_permissions']              = 'Administrative access';
 $_['text_new_permissions']          = 'New permissions:';
 
 // Error
-$_['error_install']                 = 'Warning: Install folder still exists and should be deleted for security reasons!';
-$_['error_cache']                   = 'Warning: Cache directory %s not writeable!';
-$_['error_download']                = 'Warning: Download directory %s not writeable!';
-$_['error_logs']                    = 'Warning: Log directory %s not writeable!';
+$_['error_permission']  = 'Warning: You do not have permission to modify user groups!';
 ?>
