b7cdb1796a4fbd068d8ea753ff75e9a3debd9492
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-14 17:53:05 +0200

    Update info.php

--- upload/converter/language/english/upgrade/info.php
+++ upload/converter/language/english/upgrade/info.php
@@ -2,13 +2,14 @@
 // Heading
 $_['heading_title']                 = 'Upgrade Info';
 // Text
-$_['text_upgrade_info']             = 'Upgrade Info';
 $_['text_database_info']            = 'To your database information';
-$_['text_tables_complete']          = 'All new tables is in Your Database.';
-$_['text_your_db_tables']           = 'Your database tables in total:';
-$_['text_oc2_db_tables']            = 'OC version 2.0 database tables:';
-$_['text_missing_tables']           = 'Missing tables in total:';
 $_['text_expired_tables']           = 'Expired or other tables in total:';
+$_['text_missing_tables']           = 'Missing tables in total:';
+$_['text_oc2_db_tables']            = 'OC version 2.0 database tables:';
+$_['text_tables_complete']          = 'All new tables is in Your Database.';
+$_['text_upgrade_info']             = 'Upgrade Info';
+$_['text_your_database']            = 'Your database:';
+$_['text_your_db_tables']           = 'Your database tables in total:';
 // Btn
 $_['button_database']               = 'To Upgrade Database';
 ?>
