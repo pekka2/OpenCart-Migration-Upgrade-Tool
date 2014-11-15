e264d1b31878e1a950eca440d02fb351af4efe00
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-15 14:07:44 +0200

    Create permission.php

---
+++ beta-project/upload/converter/language/finnish/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Ei oikeuksia!'; 
+
+// Text
+$_['button_permission']             = 'Ylläpitäjän käyttöoikeudet';
+$_['text_permission']               = 'Sinulla ei ole oikeuksia nähdä tätä sivua, ota tarvittaessa yhteys järjestelmän ylläpitäjään.';
+?>
