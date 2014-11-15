a8e2b4f98d799b585d463d4c3f388c38db6b8679
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-15 14:05:32 +0200

    Update home.php

--- beta-project/upload/converter/language/finnish/common/home.php
+++ beta-project/upload/converter/language/finnish/common/home.php
@@ -8,9 +8,4 @@ $_['button_language']               = 'Hallintapaneelin kieli';
 $_['button_permission']             = 'Ylläpitäjän käyttöoikeudet';
 $_['button_upgrade']                = 'Päivityksen aloitus';
 
-// Error
-$_['error_install']                 = 'Warning: Install folder still exists and should be deleted for security reasons!';
-$_['error_cache']                   = 'Warning: Cache directory %s not writeable!';
-$_['error_download']                = 'Warning: Download directory %s not writeable!';
-$_['error_logs']                    = 'Warning: Log directory %s not writeable!';
 ?>
