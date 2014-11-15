f150874a837a50b086e101b267353b7a80b2c1e5
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-15 13:57:55 +0200

    Create home.php

---
+++ beta-project/upload/converter/language/english/common/home.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Opencart Converter %s Dashboard';
+// Button
+$_['button_language']               = 'Admin Language';
+$_['button_permission']             = 'Administrative permission';
+$_['button_upgrade']                = 'Initial Upgrade';
+?>
