d048da2daa693d4c403a08b2237790770c54c91a
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-22 12:29:40 +0200

    Update configuration.php

--- upload/converter/model/upgrade/configuration.php
+++ upload/converter/model/upgrade/configuration.php
@@ -27,6 +27,7 @@ class ModelUpgradeConfiguration extends Model{
 
 			if( is_writable( DIR_DOCUMENT_ROOT . 'config.php' ) ) {
 				if( !$simulate ) {
+		    	$content = str_replace( '/system/database/', '/system/library/db/', $content );
 					$fw = fopen( DIR_DOCUMENT_ROOT . 'config.php', 'wb' );
 					fwrite( $fw, $content );
 					fclose( $fw );
@@ -63,6 +64,7 @@ class ModelUpgradeConfiguration extends Model{
 
 			if( is_writable( DIR_DOCUMENT_ROOT. $file ) ) {
 				if( !$simulate ) {
+		    	$content = str_replace( '/system/database/', '/system/library/db/', $content );
 					$fw = fopen( DIR_DOCUMENT_ROOT . $file, 'wb' );
 					fwrite( $fw, $content );
 					fclose( $fw );
