eb4625c0bd1d5b06eb1160bdfec00f40313527aa
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-28 22:28:43 +0200

    Delete header.php

--- beta-project/upload/converter/language/english/common/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']                    = 'Administration';
-
-// Text
-$_['text_your_version']                = 'FOUND YOUR STORE VERSION: %s';
-$_['text_confirm']		       = 'Delete/Uninstall cannot be undone! Are you sure you want to do this?';
-?>
