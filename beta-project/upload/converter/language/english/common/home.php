052047d6d50ef1376a4fc11516ba6ae0ff02cbb9
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-11-28 22:28:53 +0200

    Delete home.php

--- beta-project/upload/converter/language/english/common/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title']                 = 'Opencart Converter %s Dashboard';
-// Button
-$_['button_language']               = 'Admin Language';
-$_['button_permission']             = 'Administrative permission';
-$_['button_upgrade']                = 'Initial Upgrade';
-?>
