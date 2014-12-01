bf53d331ec6282b9466f97e80cf06206e16d0e7f
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-12-01 22:32:26 +0200

    Create user.php

---
+++ beta/converter/language/finnish/common/user.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Aseta ylläpitäjän käyttöoikeudet';
+
+// Text
+$_['button_upgrade']                = 'Päivityksen aloitukseen';
+$_['text_permissions']              = 'Ylläpitäjän oikeudet';
+$_['text_new_permissions']          = 'Uudet käyttöoikeudet:';
+
+// Error
+$_['error_permission']              = 'Varoitus: sinulla ei ole oikeuksia muokata käyttäjäryhmiä!';
+
+?>
