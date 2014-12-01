5750b631eebe125341b14881a2953a384de77ed7
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-12-01 22:08:26 +0200

    Create footer.php

---
+++ beta/converter/controller/common/footer.php
@@ -0,0 +1,18 @@
+<?php
+class ControllerCommonFooter extends Controller {   
+	protected function index() {
+		$this->language->load('common/footer');
+
+		$this->data['text_footer'] = sprintf($this->language->get('text_footer'), VERSION);
+		$this->data['text_to_my_store'] = $this->language->get('text_to_my_store');
+
+		if (file_exists(DIR_SYSTEM . 'config/svn/svn.ver')) {
+			$this->data['text_footer'] .= '.r' . trim(file_get_contents(DIR_SYSTEM . 'config/svn/svn.ver'));
+		}
+
+		$this->template = 'common/footer.tpl';
+
+		$this->render();
+	}
+}
+?>
