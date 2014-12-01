b8fa1a77c944a5b78fc873296493eb40947ce9f3
Pekka Mansikka <pekka2@users.noreply.github.com>
2014-12-01 22:28:25 +0200

    Create images.php

---
+++ beta/converter/language/english/upgrade/images.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']             = 'Copying Image Files and Upgrade Image paths';
+
+// Text
+$_['text_error']		= 'Error';
+$_['text_finish']		= 'Finish';
+$_['text_finish_note']  	= '<b>Note</b>: do not forget to delete the folder <b>converter</b> afterwards!!';
+$_['text_finish_text']	        = 'Congratulation!<br />Your old shop is successfully updated to the latest version. Please use the buttons below to navigate further.';
+$_['text_off']			= 'OFF';
+$_['text_on']			= 'ON';
+$_['text_simulation']	        = 'Simulation is';
+$_['text_upgrade_images']       = 'Upgrade of Imagepaths';
+$_['text_upgrade_info']         = 'Upgrade Info';
+// msg
+$_['msg_change_path']    	= 'CHANGE total <b>%s</b> image filepath(s) in database successfully';
+$_['msg_copy_image']	        = 'COPY total <b>%s</b> image(s) files successfully';
+$_['msg_errors']	= 'Directory <b>%s</b> is not found! You check path.';
+$_['msg_image_copied']	        = 'Image file <b>%s</b> successfully copied';
+$_['msg_image_path']	        = 'imagepath successfully changed';
+$_['msg_image_skipped']  	= 'Adopting image paths skipped';
+$_['msg_new_dir']		= 'ADDED total <b>%s</b> new directories successfully';
+$_['msg_newDir']		= 'Directory <b>%s</b> successfully created';
+$_['msg_perm_dir']		= 'No writing permission to directory <b>%s</b>. To procceed change first (either 0755 or 0777)';
+$_['msg_renamed_dir']	        = 'Directory <b>%s</b> renamed successfully to <b>%s</b>';
+$_['msg_renamed_total_dir']	= 'RENAMED total <b>%s</b> directories successfully';
+?>
