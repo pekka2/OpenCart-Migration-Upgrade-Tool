47a83ae62abe6b08e3c17467834801b6309d18c4
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-02-13 21:21:15 +0200

    Create login.php

---
+++ upload/converter/language/fi-FI/common/login.php
@@ -0,0 +1,20 @@
+<?php
+// header
+$_['heading_title']  = 'Ylläpito';
+
+// Text
+$_['text_heading']   = 'Ylläpito';
+$_['text_login']     = 'Syötä kirjautumistiedot.';
+$_['text_forgotten'] = 'Unohdin salasanani';
+
+// Entry
+$_['entry_username'] = 'Käyttäjänimi:';
+$_['entry_password'] = 'Salasana:';
+
+// Button
+$_['button_login']   = 'Kirjaudu';
+
+// Error
+$_['error_login']    = 'Käyttäjänimi ja/tai salasana eivät täsmää.';
+$_['error_token']    = 'Istunnon valtuutus ei kelpaa, kirjaudu uudelleen.';
+?>
