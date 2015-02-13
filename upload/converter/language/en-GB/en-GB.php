86ce761e3b6c4e981d1b27c259507188f5f3871d
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-02-13 21:16:39 +0200

    Create en-GB.php

---
+++ upload/converter/language/en-GB/en-GB.php
@@ -0,0 +1,10 @@
+<?php
+// Locale
+$_['code']                          = 'en';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Home';
+$_['button_save']                   = 'Save';
+
+?>
