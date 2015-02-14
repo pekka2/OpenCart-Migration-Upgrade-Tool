03f04a3b2df2b7a045f54738d59320a60526dac8
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-02-14 13:14:09 +0200

    Delete permission.php

--- beta/converter/language/de-DE/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Heading
-$_['heading_title']		= 'Zugriff verweigert!';
-
-// Text
-$_['button_permission']        = 'Administrative permission';
-$_['text_permission']	       = 'Keine Rechte für den Zugriff auf diese Seite, bitte Admin kontaktieren.';
