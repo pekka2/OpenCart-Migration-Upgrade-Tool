2b7a622cb3c636704695eb57c60f18f8f1ecdc03
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-02-17 10:29:35 +0200

    Update home.php

--- upload/converter/language/en-US/common/home.php
+++ upload/converter/language/en-US/common/home.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']                 = 'Opencart Converter %s Dashboard';
+$_['heading_title']                 = 'OpenCart Migration & Upgrade Tool %s';
 // Button
 $_['button_language']               = 'Admin Language';
 $_['button_permission']             = 'Administrative permission';
