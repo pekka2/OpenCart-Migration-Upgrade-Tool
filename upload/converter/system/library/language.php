a9840f6359af033084508b045b2b7648707c9e94
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-06-05 22:56:10 +0300

    Update language.php

--- upload/converter/system/library/language.php
+++ upload/converter/system/library/language.php
@@ -18,18 +18,9 @@ class Language {
                                $language = explode(';',$browser_languages[2]);
                         } elseif( isset($browser_languages[1]) ) {
                                 $language = explode(';',$browser_languages[1]);
-                         } elseif( isset($browser_languages[0]) ) {
-                                $language = $browser_languages[0];
                          } else {
-                             $browser_languages2 = explode( ';', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-                              if( isset($browser_languages2[2]) ){
-                                      $language = explode(';',$browser_languages2[2]);
-                               } elseif( isset($browser_languages2[1]) ) {
-                                       $language = explode(';',$browser_languages2[1]);
-                               } else {
-                                       $language = $browser_languages2[0]);
-                               }
-                    }
+                                $language = $browser_languages[0];
+                         } 
                         $arr_language = explode('-',$language[0]);
                         if(isset($arr_language[1])){
                                 $arr_language[1] = strtoupper($arr_language[1]);
