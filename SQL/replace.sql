f02d7c3236dc46e9a6c89aef6ce1f5862c62556e
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-06-13 18:50:33 +0300

    Create replace.sql

---
+++ SQL/replace.sql
@@ -0,0 +1,6 @@
+UPDATE `oc_banner_image` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
+UPDATE `oc_category` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
+UPDATE `oc_product` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
+UPDATE `oc_product_image` SET `image` =   REPLACE ( image , 'data/', 'catalog' );
+UPDATE `oc_setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_logo';
+UPDATE `oc_setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_icon';
