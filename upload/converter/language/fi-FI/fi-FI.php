3ee58dc001b408ad308be36f1d51ea63f4e5bea4
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-07-03 21:52:59 +0300

    Update fi-FI.php

--- upload/converter/language/fi-FI/fi-FI.php
+++ upload/converter/language/fi-FI/fi-FI.php
@@ -1,8 +1,23 @@
 <?php
 
-$_['code']			= 'fi';
-$_['name']			= 'Finnish(FI)';
-$_['direction']			= 'ltr';
-$_['button_save']               = 'Tallenna';
-$_['text_home']               = 'Etusivu';
+$_['code']                          = 'fi-FI';
+$_['direction']                     = 'ltr';
 
+// Text
+$_['text_home']                     = 'Etusivu';
+
+// Button
+$_['button_database']               = 'Päivitä tietokanta';
+$_['button_permission']             = 'Ylläpitäjän käyttöoikeudet';
+$_['button_save']                   = 'Tallenna';
+$_['button_upgrade']                = 'Päivityksen aloitus';
+
+// Btn
+$_['btn_skip']		= 'Ohita';
+$_['btn_continue']	= 'Jatka';
+
+// Entry
+$_['entry_perms']		= 'Sinun palvelimen hakemistojen kirjoitusoikeus';
+// Help
+$_['help_perms']		= '(hakemiston <b>system/logs</b> kirjoitusoikeus)';
+?>
