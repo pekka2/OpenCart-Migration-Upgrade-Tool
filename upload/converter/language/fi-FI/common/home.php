2c7b2024cddc1465b0c6292595c5fa2764841f2d
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-07-03 21:51:46 +0300

    Update home.php

--- upload/converter/language/fi-FI/common/home.php
+++ upload/converter/language/fi-FI/common/home.php
@@ -1,16 +1,4 @@
 <?php
 // Heading
 $_['heading_title']                 = 'OpenCart Migration & Upgrade Tool %s';
-
-// Text
-// Button
-$_['button_language']               = 'Hallintapaneelin kieli';
-$_['button_permission']             = 'Ylläpitäjän käyttöoikeudet';
-$_['button_upgrade']                = 'Päivityksen aloitus';
-
-// Error
-$_['error_install']                 = 'Warning: Install folder still exists and should be deleted for security reasons!';
-$_['error_cache']                   = 'Warning: Cache directory %s not writeable!';
-$_['error_download']                = 'Warning: Download directory %s not writeable!';
-$_['error_logs']                    = 'Warning: Log directory %s not writeable!';
 ?>
