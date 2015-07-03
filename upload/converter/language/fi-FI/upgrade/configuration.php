660091ebc15069296736493a15e404ebda068212
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-07-03 21:55:21 +0300

    Create configuration.php

---
+++ upload/converter/language/fi-FI/upgrade/configuration.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']             = 'Muokkaa konfigurointi tiedostoja';
+
+// Text
+$_['text_curr_setting']	        = '(nykyinen asetus)';
+$_['text_images_info']          = 'Päivitä kuvatiedostojen polut';
+$_['text_intro_3']		= 'Tässä muuntajassa voit käyttää myös <b>simulointi</b> eli päivityksen esikatselutilaa.
+';
+$_['text_intro_step_3']	        = 'Tämä nimeää <b>image/data</b> hakemistot sekä vaihtaa kuvatiedostojen polut tietokannassa.';
+$_['text_off']			= 'POIS PÄÄLTÄ';
+$_['text_on']			= 'PÄÄLLÄ';
+$_['text_upgrade_info']         = 'Kofigurointi asetusten päivitys';
+$_['text_simulation']	        = 'Testitila on';
+$_['text_step_3_3']		= 'Vaihe 3 / 3';
+$_['text_update_config']        = 'Nyt voit myös päivittää  <b>config.php</b> tiedostoja, jos sitä ei ole vielä tehty';
+// Header
+$_['header_step_3']		= 'Säädä kuvatiedostojen polut';
+// msg
+$_['msg_config_added']  	= 'Asetus <b>%s</b> on onnistuneesti Lisätty <b>%s</b> tauluun';
+$_['msg_config_constant']	= 'Asetus <b>%s</b>  lisätty onnistuneesti tiedostoon <b>%s</b>!';
+$_['msg_config_replace']        = 'Asetustiedoston  <b>%s</b> polku määritelty';
+$_['msg_config_skipped']        = 'Asennustiedostot pyynnöstäsi ohitettu';
+$_['msg_config_uptodate']	= 'Asetustiedosto <b>%s</b> on päivitetty';
+$_['msg_perm_file']		= 'Ei oikeuksia kirjoittaa tiedostoon <b>%s</b>, muokkaa käyttöoikeudeksi joko 644 tai 666';
+?>
