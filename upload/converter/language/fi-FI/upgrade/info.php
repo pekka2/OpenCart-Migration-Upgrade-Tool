c965eab9ae752eb1baf5aaa81120e479be16e6cf
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-07-03 21:56:14 +0300

    Create info.php

---
+++ upload/converter/language/fi-FI/upgrade/info.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Päivitys Info';
+// Text
+$_['text_home']                     = 'Koti';
+$_['text_database_info']            = 'Sinun tietokannan tiedot';
+$_['text_expired_tables']           = 'Vanhentuneita tai muita tauluja yhteensä:';
+$_['text_missing_tables']           = 'Puuttuvia tauluja yhteensä:';
+$_['text_oc2_db_tables']            = 'Tauluja OC version 2.0 tietokannassa:';
+$_['text_tables_complete']          = 'Kaikki version 2.0 taulut on sinun tietokannassa.';
+$_['text_upgrade_info']             = 'Päivitys Info';
+$_['text_your_database']            = 'Sinun tietokanta:';
+$_['text_your_db_tables']           = 'Tauluja sinun tietokannassa yhteensä:';
+?>
