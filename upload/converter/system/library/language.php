dfb1e5b62b5b202b36fc0d39bfa44ccc73d708cc
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-07-04 18:30:02 +0300

    Update language.php

--- upload/converter/system/library/language.php
+++ upload/converter/system/library/language.php
@@ -7,22 +7,49 @@ class Language {
 	public function __construct($directory) {
 		$this->directory = $directory;
 	}
-
 	public function get($key) {
 		return (isset($this->data[$key]) ? $this->data[$key] : $key);
                                               $this->getLang();
 	}
 	public function getLang() {
-                        $browser_languages = explode( ',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-                      
-                        if(is_dir(DIR_LANGUAGE . $browser_languages[0])){
-                             $this->lang = $browser_languages[0];
+                        $browser_language = $this->http_language();
+                        $languages = $$browser_language[0];
+
+                        if( is_dir( DIR_LANGUAGE . $languages[0] ) ){
+                             $this->lang = $languages[0];
+                        } elseif ( isset($browser_language[2]) ) {
+                             $languages2 = explode(';',$browser_language[2][0]);
+                             if( is_dir( DIR_LANGUAGE . $languages2[0] ) && $languages2[0] != $this->default ){
+                                $this->lang = $languages2[0];
+                              } else {
+                                $this->lang = $this->default;
+                              }
+                        } elseif ( isset($browser_language[1]) ) {
+                            $languages3 = explode(';',$browser_language[1][0]);
+                             if( is_dir( DIR_LANGUAGE . $languages3[0] ) && $languages3[0] != $this->default ){
+                               $this->lang = $languages3[0];
+                              } else {
+                               $this->lang = $this->default;
+                              }
                         } else {
-                             $this->lang = $this->default;
+                               $this->lang = $this->default;
                         }
                       return $this->lang;
 	}
+        public function http_language(){
+            $array = array();
+                foreach (explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $lang) {
+                        $pattern = '/^(?P<primarytag>[a-zA-Z]{2,8})'.
+                                   '(?:-(?P<subtag>[a-zA-Z]{2,8}))?(?:(?:;q=)'.
+                                   '(?P<quantifier>\d\.\d))?$/';
+                         $splits = array();
 
+                  if (preg_match($pattern, $lang, $splits)) {
+                     $array[] = $splits;
+                   } 
+               }
+              return $array;
+        }
 	public function load($filename) {
                 $language = $this->getLang();
 		$file = DIR_LANGUAGE . $language . '/' . $filename . '.php';
