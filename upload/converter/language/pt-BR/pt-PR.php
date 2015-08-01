c8274eca7a0636f87da90e9adaf28aaf5cd15193
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:16:02 +0300

    Create pt-PR.php

---
+++ upload/converter/language/pt-BR/pt-PR.php
@@ -0,0 +1,23 @@
+<?php
+// Locale
+$_['code']                          = 'pt-BR';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Home';
+
+// Button
+$_['button_database']               = 'Para atualizar o banco de dados';
+$_['button_permission']             = 'Permissão administrativa';
+$_['button_save']                   = 'Salvar';
+$_['button_upgrade']                = 'Iniciar atualização';
+
+// Btn
+$_['btn_skip']		= 'Ignorar';
+$_['btn_continue']	= 'Continuar';
+
+// Entry
+$_['entry_perms']		= 'Sua permissão de escrita no servidor';
+// Help
+$_['help_perms']		= '(permissão de escrita do diretório <b>system/logs</b>)';
+?>
