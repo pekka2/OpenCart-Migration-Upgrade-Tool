1a54bfea798450381b0666d7a0da305f6b288af4
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:14:25 +0300

    Create images.php

---
+++ upload/converter/language/nl-NL/upgrade/images.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']             = 'Afbeeldingsbestanden kopiëren en de afbeelding upgradepaden';
+
+// Text
+$_['text_home']                     = 'Home';
+$_['text_error']		= 'Fout';
+$_['text_finish']		= 'Voltooien';
+$_['text_finish_note']  	= '</b>Opmerking:</b> Vergeet niet de map <b>converter</b> te verwijderen!';
+$_['text_finish_text']	        = 'Hoera! < br / > uw oude winkel is bijgewerkt naar de nieuwste versie. Gebruik de knoppen hieronder om te navigeren verder.';
+$_['text_off']			= 'UIT';
+$_['text_on']			= 'AAN';
+$_['text_simulation']	        = 'Simulatie is';
+$_['text_upgrade_images']       = 'Upgrade van Imagepaths';
+$_['text_upgrade_info']         = 'Upgrade Informatie';
+// msg
+$_['msg_change_path']    	= 'Totale <b>%s</b> afbeelding filepath (s) in de database met succes wijzigen';
+$_['msg_copy_image']	        = 'Totale <b>%s</b>-afbeeldingsbestanden (s) met succes kopiëren';
+$_['msg_errors']	= 'Map <b>%s</b> is niet gevonden! U Controleer uw pad.';
+$_['msg_image_copied']	        = 'Image bestand <b>%s</b> met succes gekopieerd';
+$_['msg_image_path']	        = 'Image map met succes gewijzigd';
+$_['msg_image_skipped']  	= 'Overnemen van de afbeeldingspaden overgeslagen';
+$_['msg_new_dir']		= 'Totale <b>%s</b> de nieuwe mappen met succes toegevoegd';
+$_['msg_newDir']		= 'Map <b>%s</b> met succes gemaakt';
+$_['msg_perm_dir']		= 'Goed schrijven toestemming naar de map <b>%s</b>. Om verder te gaan verander eerst (0755 of 0777)';
+$_['msg_renamed_dir']	        = 'Map <b>%s</b> met succes hernoemd naar <b>%s</b>';
+$_['msg_renamed_total_dir']	= 'Totale <b>%s</b> mappen HERNOEMD met succes';
+?>
