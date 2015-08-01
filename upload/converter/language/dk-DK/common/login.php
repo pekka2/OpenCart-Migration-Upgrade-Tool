0e695e93c1c48d697410c1bff83f4614b45818c0
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:57:41 +0300

    Create login.php

---
+++ upload/converter/language/dk-DK/common/login.php
@@ -0,0 +1,19 @@
+<?php
+// header
+$_['heading_title']  = 'Administration';
+
+// Text
+$_['text_heading']   = 'Administration';
+$_['text_login']     = 'Indtast dine login-oplysninger.';
+
+// Entry
+$_['entry_username'] = 'Brugernavn:';
+$_['entry_password'] = 'Adgangskode:';
+
+// Button
+$_['button_login']   = 'Login';
+
+// Error
+$_['error_login']    = 'Ingen match for brugernavnet og/eller adgangskode.';
+$_['error_token']    = 'Ugyldigt token i sessionen. Venligst login igen.';
+?>
