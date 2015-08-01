0b21f4599616fa127e60596f44c474c5b4dc03dc
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:28:24 +0300

    Create images.php

---
+++ upload/converter/language/tr-TR/upgrade/images.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']             = 'Resim dosyaları kopyalanıyor ve resim yolları yükseltiliyor';
+
+// Text
+$_['text_home']                     = 'Ana sayfa';
+$_['text_error']		= 'Hata';
+$_['text_finish']		= 'Bitiş';
+$_['text_finish_note']  	= '<b>Not</b>: Daha sonra <b>converter</b> klasörünü silmeyin unutmayın!!';
+$_['text_finish_text']	        = 'Tebrikler! <br/> Eski mağazanız başarıyla en son sürüme güncelleştirildi. Aşağıdaki butonları sonrası için kullanın.';
+$_['text_off']			= 'KAPALI';
+$_['text_on']			= 'AÇIK';
+$_['text_simulation']	        = 'Simülasyon';
+$_['text_upgrade_images']       = 'Resim Yollarını Yükseltme';
+$_['text_upgrade_info']         = 'Yükseltme Bilgisi';
+// msg
+$_['msg_change_path']    	= 'Toplam <b>%s</b> resim dosya yolu veritabanında başarıyla değiştirildi';
+$_['msg_copy_image']	        = 'Toplam <b>%s</b> resim dosyası başarıyla KOPYALANDI';
+$_['msg_errors']	= 'Dizin <b>%s</b> bulunamadı! Yolunu denetleyin.';
+$_['msg_image_copied']	        = 'Resim dosyası <b>%s</b> başarıyla kopyalandı';
+$_['msg_image_path']	        = 'resim yolu başarıyla değiştirildi';
+$_['msg_image_skipped']  	= 'Edinilen resim yolları atlandı';
+$_['msg_new_dir']		= 'Toplam <b>%s</b> yeni dizin başarıyla eklendi';
+$_['msg_newDir']		= 'Dizin <b>%s</b> başarıyla oluşturuldu';
+$_['msg_perm_dir']		= '<b>%s</b> klasöründe yazma izni yok. Devam etmek için öncelikle (0755 yada 0777) değiştirin';
+$_['msg_renamed_dir']	        = 'Dizin <b>%s</b> başarıyla <b>%s</b> olarak yeniden adlandırıldı';
+$_['msg_renamed_total_dir']	= 'Toplam <b>%s</b> dizinleri başarıyla yeniden ADLANDIRILDI';
+?>
