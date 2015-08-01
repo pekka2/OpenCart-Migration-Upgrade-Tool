ab5c62c6d3dc0b00f95fa44ee89567e1cf986e67
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:58:50 +0300

    Create not_found.php

---
+++ upload/converter/language/dk-DK/error/not_found.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'Siden blev ikke fundet!';
+
+// Text
+$_['text_not_found'] = 'Siden du søger kan ikke findes! Kontakt administratoren, hvis problemet fortsætter.';
+?>
