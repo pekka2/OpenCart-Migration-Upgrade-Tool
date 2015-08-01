8c6ce3b922f5139600c44fb08a89baf1d081f7d6
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:21:16 +0300

    Create images.php

---
+++ upload/converter/language/pt-BR/upgrade/images.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']             = 'Copiando arquivos de imagem e atualizar os caminhos das imagens';
+
+// Text
+$_['text_home']                     = 'Home';
+$_['text_error']		= 'Erro';
+$_['text_finish']		= 'Finalizar';
+$_['text_finish_note']  	= '<b>Aviso</b>: não se esqueça de apagar a pasta <b>converter</b> depois!';
+$_['text_finish_text']	        = 'Parabéns! <br /> Sua loja antiga foi atualizada com êxito para a versão mais recente. Por favor, use os botões abaixo para navegar mais.';
+$_['text_off']			= 'Desligado';
+$_['text_on']			= 'Ligado';
+$_['text_simulation']	        = 'Simulador está';
+$_['text_upgrade_images']       = 'Atualização do caminho das imagens';
+$_['text_upgrade_info']         = 'Info de atualização';
+// msg
+$_['msg_change_path']    	= 'Total de <b>%s</b> caminhos de imagens alterados com sucesso no banco de dados.';
+$_['msg_copy_image']	        = 'Total de <b>%s</b> arquivos de imagens copiados com sucesso';
+$_['msg_errors']	= 'Diretório <b>%s</b> não encontrado! Verifique o caminho.';
+$_['msg_image_copied']	        = 'Arquivo de imagem <b>%s</b> copiado com sucesso';
+$_['msg_image_path']	        = 'ImagePath alterado com êxito';
+$_['msg_image_skipped']  	= 'Adoção de caminhos de imagem ignorada';
+$_['msg_new_dir']		= 'Total de <b>%s</b> novos diretórios ADICIONADOS com sucesso';
+$_['msg_newDir']		= 'Diretório <b>%s</b> criado com êxito';
+$_['msg_perm_dir']		= 'Sem permissão de escrita no diretório <b>%s</b>. Para continuar mude as permissões para 0755 ou 0777';
+$_['msg_renamed_dir']	        = 'Diretório <b>%s</b> renomeado com êxito para <b>%s</b>';
+$_['msg_renamed_total_dir']	= 'Total de <b>%s</b> diretórios RENOMEADOS com êxito';
+?>
