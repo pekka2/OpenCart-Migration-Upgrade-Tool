3b51a6e88140f8b45cf90444092bdb83fc10d0c0
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:26:01 +0300

    Create user.php

---
+++ upload/converter/language/tr-TR/common/user.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Tam yönetici izinleri ayarlama';
+
+// Text
+$_['button_upgrade']                = 'Güncelleştirmeye Başla';
+$_['text_permissions']              = 'Yönetim İzni';
+$_['text_new_permissions']          = 'Yeni izinler:';
+
+// Error
+$_['error_permission']  = 'Uyarı: Kullanıcı gruplarını değiştirme iznine sahip değilsiniz!';
+?>
