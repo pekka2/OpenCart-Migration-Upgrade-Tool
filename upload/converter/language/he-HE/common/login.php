2185d4343496b974ef0c2db0affca3bd98c70144
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:04:24 +0300

    Create login.php

---
+++ upload/converter/language/he-HE/common/login.php
@@ -0,0 +1,19 @@
+<?php
+// header
+$_['heading_title']  = 'הנהלה';
+
+// Text
+$_['text_heading']   = 'Administration';
+$_['text_login']     = 'אנא הזן את פרטי ההתחברות שלך.';
+
+// Entry
+$_['entry_username'] = 'שם משתמש:';
+$_['entry_password'] = 'סיסמה:';
+
+// Button
+$_['button_login']   = 'כניסה';
+
+// Error
+$_['error_login']    = 'שם משתמש ו/או סיסמה לא תואמים.';
+$_['error_token']    = 'Invalid token session. Please login again.';
+?>
