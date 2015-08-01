2829ad62bc9055554185198042d3c4424f9f7843
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:27:35 +0300

    Create configuration.php

---
+++ upload/converter/language/tr-TR/upgrade/configuration.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']             = 'Yapılandırma Dosyalarını Düzenle';
+
+// Text
+$_['text_curr_setting']	        = '(geçerli ayar)';
+$_['text_images_info']          = 'Resimm yollarını yükseltme';
+$_['text_intro_3']		= 'Bu Güncelleyici <b>simülasyon</b> modunda da işletilebilir. Bu seçeneği <br/> etkinleştirirseniz gerçek hiçbir işlem yapılmaz.';
+$_['text_intro_step_3']	        = 'Bu işlem <b>image/data</b> dizinini yeniden adlandıracak ve resim yollarını veritabanında güncelleştirecek';
+$_['text_off']			= 'KAPALI';
+$_['text_on']			= 'AÇIK';
+$_['text_upgrade_info']         = 'Yapılandırma Yükseltme';
+$_['text_simulation']	        = 'Simülasyon';
+$_['text_step_3_3']		= 'Adım 3/3';
+$_['text_update_config']        = 'Şimdi de <b>config. php</b> dosyaları güncelleyebilirsiniz eğer bitmemişse';
+// Header
+$_['header_step_3']		= 'Resim yollarını ayarlama';
+// msg
+$_['msg_config_added']  	= '<b>%s</b> ayarı başarıyla şuna eklendi <b>%s</b>';
+$_['msg_config_constant']	= '<b>%s</b> ayarı başarıyla şuna eklendi <b>%s</b>!';
+$_['msg_config_replace']        = 'Ayar dosyasındaki dosya yolu <b>%s</b> ayarlandı';
+$_['msg_config_skipped']        = 'Edinilen yapılandırma dosyaları atlandı';
+$_['msg_config_uptodate']	= 'Yapılandırma dosyası <b>%s</b> zaten güncel';
+$_['msg_perm_file']		= '<b>%s</b> dosyası yazma izni yok, devam etmek için ilk önce izinleri 644 olarak veya 666 olarak değiştirin';
+?>
