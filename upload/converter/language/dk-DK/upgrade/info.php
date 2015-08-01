2b6a9f17a97dfea44a1c93ccf127c029c60ec4f5
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:01:15 +0300

    Create info.php

---
+++ upload/converter/language/dk-DK/upgrade/info.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Opgradere Info';
+// Text
+$_['text_database_info']            = 'Til din databaseoplysninger';
+$_['text_expired_tables']           = 'Udløbet eller andre tabeller i alt:';
+$_['text_missing_tables']           = 'Manglende tabeller i alt:';
+$_['text_oc2_db_tables']            = 'OC version 2.0 databasetabeller:';
+$_['text_tables_complete']          = 'Alle nye tabeller er i din Database.';
+$_['text_upgrade_info']             = 'Opgradere Info';
+$_['text_your_database']            = 'Din database:';
+$_['text_your_db_tables']           = 'Dine databasetabeller i alt:';
+?>
