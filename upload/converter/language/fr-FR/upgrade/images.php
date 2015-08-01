974097aad23077c07d5ac9fe8bcc92c9778f3b01
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:52:34 +0300

    Create images.php

---
+++ upload/converter/language/fr-FR/upgrade/images.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']             = 'Copie des fichiers d\'images et mise à niveau des chemins d\'image';
+
+// Text
+$_['text_error']		= 'Erreur';
+$_['text_finish']		= 'Finition';
+$_['text_finish_note']  	= '<b>Note</b> : N\'oubliez pas de supprimer le fichier <b>converter</b> par la suite !!';
+$_['text_finish_text']	        = 'Félicitations!<br />Votre ancien magasin à été mis à jour avec succès vers la dernière version. Veuillez utiliser les boutons ci-dessous pour naviguer plus loin.';
+$_['text_upgrade_images']       = 'Mise à jour des chemins d\'image';
+$_['text_upgrade_info']         = 'Informations de mise à jour';
+// msg
+$_['msg_change_path']    	= 'CHANGEMENT total de <b>%s</b> chemin(s) de fichier images dans la base de données réalisé avec succès';
+$_['msg_copy_image']	        = 'COPIE total de <b>%s</b> fichier(s) images réalisée avec succès';
+$_['msg_errors']	= 'Le répertoire <b>%s</b> est introuvable! Vérifiez le chemin.';
+$_['msg_image_copied']	        = 'Le fichier image <b>%s</b> à été copiée avec succès';
+$_['msg_image_path']	        = 'Chemin de fichier image modifié avec succès';
+$_['msg_image_skipped']  	= 'L\'adoption des chemins d\'image à été sautée';
+$_['msg_new_dir']		= 'AJOUT total de <b>%s</b> nouveaux répertoires réalisé avec succès';
+$_['msg_newDir']		= 'Répertoire <b>%s</b> créé avec succès';
+$_['msg_perm_dir']		= 'Vous n\'avez pas la permissions d\'écrire dans le répertoire <b>%s</b>. Pour procéder à la modification, veuillez changer la permission (soit 0755 ou 0777)';
+$_['msg_renamed_dir']	        = 'Le répertoire <b>%s</b> à été renommé avec succès en <b>%s</b>';
+$_['msg_renamed_total_dir']	= 'Total de <b>%s</b> répertoires RENOMMÉS avec succès';
+?>
