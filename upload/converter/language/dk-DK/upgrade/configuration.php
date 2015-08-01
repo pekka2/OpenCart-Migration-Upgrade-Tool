a43b1376338921ac76c1e28b6374a7e0d3569657
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:59:49 +0300

    Create configuration.php

---
+++ upload/converter/language/dk-DK/upgrade/configuration.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']             = 'Redigering af konfigurationsfiler';
+
+// Text
+$_['text_curr_setting']	        = '(aktuel indstilling)';
+$_['text_images_info']          = 'Opgradering af Spejl-stier';
+$_['text_intro_3']		= 'Opdateringsværktøjet kan køres i en <b>simulationstilstand</b> også.<br/>Aktivér tilstanden og ingen reelle handlinger bliver foretaget.';
+$_['text_intro_step_3']	        = 'Dette vil omdøbe mapperne <b>spejl/data</b> og opdatere spejlstier i database';
+$_['text_upgrade_info']         = 'Opgradering af konfiguration';
+$_['text_step_3_3']		= 'Trin 3/3';
+$_['text_update_config']        = 'Nu kan du også opdatere <b>config. php</b>-filer, hvis dette ikke allerede er gjort';
+// Header
+$_['header_step_3']		= 'Tilret spejl-stier';
+// msg
+$_['msg_config_added']  	= 'Indstilling <b>%s</b> føjet til <b>%s</b>';
+$_['msg_config_constant']	= 'Indstilling <b>%s</b>  føjet til <b>%s</b>!';
+$_['msg_config_replace']        = 'Filsti i konfigurationsfilen <b>%s</b> tilrettet';
+$_['msg_config_skipped']        = 'Vedtage konfigurationsfiler oversprunget';
+$_['msg_config_uptodate']	= 'Konfigurationsfil <b>%s</b> allerede opdateret';
+$_['msg_perm_file']		= 'Ingen skriveadgang fil <b>%s</b>. For at fortsætte, ændr første filtilladelsen til 644 eller 666';
+?>
