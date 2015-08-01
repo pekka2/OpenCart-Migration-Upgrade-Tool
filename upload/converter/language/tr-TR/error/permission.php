5cc881f1614ee23fcf6c1c5999749730d95e175c
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:27:01 +0300

    Create permission.php

---
+++ upload/converter/language/tr-TR/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'İzin Engellendi!'; 
+
+// Text
+$_['button_permission']     = 'Yönetim İzni';
+$_['text_permission']       = 'Bu sayfaya erişim izniniz yok, lütfen sistem yöneticinize başvurun.';
+?>
