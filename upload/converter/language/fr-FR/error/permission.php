2fa017113c2fb84834df1669052de92b59e39838
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:50:50 +0300

    Create permission.php

---
+++ upload/converter/language/fr-FR/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Autorisation refusée !'; 
+
+// Text
+$_['button_permission']     = 'Autorisation d\'administration';
+$_['text_permission']       = 'Vous n\'êtes pas autorisé à accéder à cette page, veuillez vous référer à votre administrateur système.';
+?>
