0e662d6c4e6d416a44b14a37f0ae09259d58bece
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:08:14 +0300

    Create info.php

---
+++ upload/converter/language/he-HE/upgrade/info.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']                 = 'פרטי שדרוג';
+// Text
+$_['text_database_info']            = 'למידע על מסד הנתונים שלך';
+$_['text_expired_tables']           = 'Expired or other tables in total:';
+$_['text_missing_tables']           = 'Missing tables in total:';
+$_['text_oc2_db_tables']            = 'OC version 2.0 database tables:';
+$_['text_tables_complete']          = 'All new tables is in Your Database.';
+$_['text_upgrade_info']             = 'פרטי שדרוג';
+$_['text_your_database']            = 'מסד הנתונים שלך:';
+$_['text_your_db_tables']           = 'Your database tables in total:';
+?>
