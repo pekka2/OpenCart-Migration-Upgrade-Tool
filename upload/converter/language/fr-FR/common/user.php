c3d216dd59b00fa0f5fb2f5bf90ecb2dc5473968
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:49:05 +0300

    Create user.php

---
+++ upload/converter/language/fr-FR/common/user.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Définir une autorisation d\'administration complète';
+
+// Text
+$_['button_upgrade']                = 'Mise à niveau initiale';
+$_['text_permissions']              = 'Autorisation d\'administration';
+$_['text_new_permissions']          = 'Nouvelles autorisations :';
+
+// Error
+$_['error_permission']  = 'Avertissement : Vous n\'êtes pas autorisé à modifier des groupes d\'utilisateurs !';
+?>
