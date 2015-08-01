3a656b3e16d9c8eb19205bdbfd3bfaf88ae3bb70
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:02:11 +0300

    Create he-HE.php

---
+++ upload/converter/language/he-HE/he-HE.php
@@ -0,0 +1,26 @@
+<?php
+// Locale
+$_['code']                          = 'אנגלית-ארה"ב';
+$_['direction']                     = 'משמאל לימין';
+
+// Text
+$_['text_home']                     = 'בית';
+$_['text_off']			    = 'כיבוי';
+$_['text_on']			    = 'הפעלה';
+$_['text_simulation']	        = 'הדמיה היא';
+
+// Button
+$_['button_database']               = 'לשדרוג מסד הנתונים';
+$_['button_permission']             = 'הרשאה מנהלית';
+$_['button_save']                   = 'שמור';
+$_['button_upgrade']                = 'שדרוג ראשוני';
+
+// Btn
+$_['btn_skip']		= 'דלג';
+$_['btn_continue']	= 'המשך';
+
+// Entry
+$_['entry_perms']		= 'Your server writing permission';
+// Help
+$_['help_perms']		= '(write permission of directory <b> system/logs </b>)';
+?>
