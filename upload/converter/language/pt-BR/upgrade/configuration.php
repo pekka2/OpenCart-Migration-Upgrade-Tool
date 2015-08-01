3ef1a9d6378caf4fb1118e9243b28b3c939ad2f5
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:20:27 +0300

    Create configuration.php

---
+++ upload/converter/language/pt-BR/upgrade/configuration.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']             = 'Editar arquivos de configuração';
+
+// Text
+$_['text_curr_setting']	        = '(configuração atual)';
+$_['text_images_info']          = 'Atualização do caminho das imagens';
+$_['text_intro_3']		= 'Esta atualização pode ser executada em um modo de <b>simulação</b> também.<br />Habilite esta opção e nenhuma alteração será efetuada.';
+$_['text_intro_step_3']	        = 'Isto irá renomear o diretório <b>image/data</b> e atualizará o caminho das imagens no banco de dados';
+$_['text_off']			= 'Desligado';
+$_['text_on']			= 'Ligado';
+$_['text_upgrade_info']         = 'Atualização da configuração';
+$_['text_simulation']	        = 'Simulador está';
+$_['text_step_3_3']		= 'Passo 3 / 3';
+$_['text_update_config']        = 'Agora você pode atualizar os arquivos <b>config.php</b> se ainda não tiverem sido feitos';
+// Header
+$_['header_step_3']		= 'Ajustar os caminhos da imagem';
+// msg
+$_['msg_config_added']  	= 'Configuração <b>%s</b> adicionada com sucesso à <b>%s</b>';
+$_['msg_config_constant']	= 'Configuração <b>%s</b> adicionada com sucesso à <b>%s</b>!';
+$_['msg_config_replace']        = 'Caminho dos arquivos no arquivo de configuração <b>%s</b> ajustado';
+$_['msg_config_skipped']        = 'Arquivos de configurações adopting ignorados';
+$_['msg_config_uptodate']	= 'Arquivo de configuração <b>%s</b> já foi atualizado';
+$_['msg_perm_file']		= 'Sem permissão de escrita no arquivo <b>%s</b>, para continuar modifique primeiro as permissões do arquivo para 644 ou 666';
+?>
