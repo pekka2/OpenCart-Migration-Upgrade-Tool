736a85b65f374d341aecaee2bc9a80832f64cb5a
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:55:00 +0300

    Create dk-DK.php

---
+++ upload/converter/language/dk-DK/dk-DK.php
@@ -0,0 +1,26 @@
+<?php
+// Locale
+$_['code']                          = 'dk-DK';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Hjem';
+$_['text_off']			    = 'FRA';
+$_['text_on']			    = 'TIL';
+$_['text_simulation']	        = 'Simulation er';
+
+// Button
+$_['button_database']               = 'At opgradere databasen';
+$_['button_permission']             = 'Administrativ tilladelse';
+$_['button_save']                   = 'Gem';
+$_['button_upgrade']                = 'Indledende opgradering';
+
+// Btn
+$_['btn_skip']		= 'Springe';
+$_['btn_continue']	= 'Fortsat';
+
+// Entry
+$_['entry_perms']		= 'Din server skriftlig tilladelse';
+// Help
+$_['help_perms']		= '(write permission of directory <b> system/logs </b>)';
+?>
