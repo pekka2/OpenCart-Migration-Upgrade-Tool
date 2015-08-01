d04fd366408b86d4411fdfb9f856e970d0ce7daf
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:28:44 +0300

    Create info.php

---
+++ upload/converter/language/tr-TR/upgrade/info.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Yükseltme Bilgisi';
+// Text
+$_['text_home']                     = 'Ana sayfa';
+$_['text_database_info']            = 'Veritabanı Bilgileriniz';
+$_['text_expired_tables']           = 'Toplam süresi dolmuş veya diğer tablolar:';
+$_['text_missing_tables']           = 'Toplam kayıp tablolar:';
+$_['text_oc2_db_tables']            = 'OC sürüm 2.0 veritabanı tabloları:';
+$_['text_tables_complete']          = 'Tüm yeni tablolar veritabanınızda.';
+$_['text_upgrade_info']             = 'Yükseltme Bilgisi';
+$_['text_your_database']            = 'Veritabanınız:';
+$_['text_your_db_tables']           = 'Toplam Veritabanı tablolarınız:';
+?>
