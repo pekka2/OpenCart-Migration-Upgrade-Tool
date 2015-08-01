91e6305f71278db54ff02edf63626c6a57710216
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:22:55 +0300

    Create tr-TR.php

---
+++ upload/converter/language/tr-TR/tr-TR.php
@@ -0,0 +1,23 @@
+<?php
+// Locale
+$_['code']                          = 'tr-TR';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Ana sayfa';
+
+// Button
+$_['button_database']               = 'Veritabanı Yükseltme';
+$_['button_permission']             = 'Yönetici izinleri';
+$_['button_save']                   = 'Kaydet';
+$_['button_upgrade']                = 'Güncelleştirmeyi Başlat';
+
+// Btn
+$_['btn_skip']		= 'Atla';
+$_['btn_continue']	= 'Devam';
+
+// Entry
+$_['entry_perms']		= 'Sunucu yazma izinleriniz';
+// Help
+$_['help_perms']		= '(<b>system/logs</b> dizininin yazma izinleri)';
+?>
