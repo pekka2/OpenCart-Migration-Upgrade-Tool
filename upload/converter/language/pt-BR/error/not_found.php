b82c5ac4959d0e2e4b918c4e9a084db839a4e794
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:19:22 +0300

    Create not_found.php

---
+++ upload/converter/language/pt-BR/error/not_found.php
@@ -0,0 +1,7 @@
+<?php
+// Heading
+$_['heading_title'] = 'Página não encontrada!';
+
+// Text
+$_['text_not_found'] = 'A página que você está procurando não pôde ser encontrada! Por favor, contate o administrador se o problema persistir.';
+?>
