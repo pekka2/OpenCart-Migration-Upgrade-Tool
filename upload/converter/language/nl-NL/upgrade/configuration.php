c114c10ad6d9aa13c23b5dcdaa00132871c49407
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:13:34 +0300

    Create configuration.php

---
+++ upload/converter/language/nl-NL/upgrade/configuration.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']             = 'Bewerken van configuratiebestanden';
+
+// Text
+$_['text_curr_setting']	        = '(huidige instelling)';
+$_['text_images_info']          = 'Upgrade van Imagepaths';
+$_['text_intro_3']		= 'Deze updater kan ook bediend worden in een <b>simulatie</b> mode .<br /> Schakel deze optie inschakelen in en er zullen geen echte bewerkingen worden gemaakt.';
+$_['text_intro_step_3']	        = 'Dit zal de map <b>image/data</b> hernoemen en de image-map paden aanpassen';
+$_['text_off']			= 'UIT';
+$_['text_on']			= 'AAN';
+$_['text_upgrade_info']         = 'Upgrade van de configuratie';
+$_['text_simulation']	        = 'Simulatie is';
+$_['text_step_3_3']		= 'Stap 3/3';
+$_['text_update_config']        = 'Nu kunt u ook de <b>config. php</b> bestanden bijwerken indien dat nog niet is gedaan';
+// Header
+$_['header_step_3']		= 'Afbeeldingspaden aanpassen';
+// msg
+$_['msg_config_added']  	= 'Instellingen <b>%s</b> "succesvol toegevoegd aan <b>%s</b>';
+$_['msg_config_constant']	= 'Instellingen <b>%s</b> "succesvol toegevoegd aan <b>%s</b>!';
+$_['msg_config_replace']        = 'Bestandsmappen <b>%s</b> aangepast in configuratie bestand';
+$_['msg_config_skipped']        = 'Overname van de configuratiebestanden overgeslagen';
+$_['msg_config_uptodate']	= 'Configuratie bestand <b>%s</b> is al up-to-date';
+$_['msg_perm_file']		= 'Geen toestemming om te schrijven van het bestand <b>%s</b>, om door te gaan bestandsrechten omzetten in 644 of 666';
+?>
