96b652ed665c373f48c6104198220cc30689951b
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:21:41 +0300

    Create info.php

---
+++ upload/converter/language/pt-BR/upgrade/info.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Info de atualização';
+// Text
+$_['text_home']                     = 'Home';
+$_['text_database_info']            = 'A sua informação de banco de dados';
+$_['text_expired_tables']           = 'Tabelas expiradas no total:';
+$_['text_missing_tables']           = 'Tabelas que faltam no total:';
+$_['text_oc2_db_tables']            = 'As tabelas do banco de dados da versão 2.0 OC:';
+$_['text_tables_complete']          = 'Todas as novas tabelas estão em seu banco de dados.';
+$_['text_upgrade_info']             = 'Info de atualização';
+$_['text_your_database']            = 'Seu banco de dados:';
+$_['text_your_db_tables']           = 'Suas tabelas do banco de dados no total:';
+?>
