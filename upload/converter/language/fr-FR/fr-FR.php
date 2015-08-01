f50a17c629840b56c9e4ed3b07ced1193b938169
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:45:18 +0300

    Create fr-FR.php

---
+++ upload/converter/language/fr-FR/fr-FR.php
@@ -0,0 +1,26 @@
+<?php
+// Locale
+$_['code']                          = 'fr-FR';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Accueil';
+$_['text_off']			    = 'OFF';
+$_['text_on']			    = 'ON';
+$_['text_simulation']	        = 'La simulation est';
+
+// Button
+$_['button_database']               = 'Pour mettre à niveau la base de données';
+$_['button_permission']             = 'Autorisation d\'administration';
+$_['button_save']                   = 'Enregistrer';
+$_['button_upgrade']                = 'Mise à niveau initiale';
+
+// Btn
+$_['btn_skip']		= 'Passer';
+$_['btn_continue']	= 'Continuer';
+
+// Entry
+$_['entry_perms']		= 'Votre autorisation d\'écriture sur le serveur';
+// Help
+$_['help_perms']		= '(autorisation d\'écriture sur le répertoire <b>system/logs</b>)';
+?>
