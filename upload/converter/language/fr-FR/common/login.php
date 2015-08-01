62c5a01ebc754f76c3324ac473ac618d99155a85
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:48:39 +0300

    Create login.php

---
+++ upload/converter/language/fr-FR/common/login.php
@@ -0,0 +1,19 @@
+<?php
+// header
+$_['heading_title']  = 'Administration';
+
+// Text
+$_['text_heading']   = 'Administration';
+$_['text_login']     = 'Veuillez entrer vos identifiants.';
+
+// Entry
+$_['entry_username'] = 'Nom d\'utilisateur :';
+$_['entry_password'] = 'Mot de passe :';
+
+// Button
+$_['button_login']   = 'Se connecter';
+
+// Error
+$_['error_login']    = 'Pas de concordance pour le nom d\'utilisateur et/ou mot de passe.';
+$_['error_token']    = 'Jeton de session invalide. Veuillez vous reconnecter.';
+?>
