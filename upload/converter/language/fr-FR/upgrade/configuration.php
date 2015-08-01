23d4b0f1f3632085ad784b8b9c08a0fc76c0b7f7
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:51:39 +0300

    Create configuration.php

---
+++ upload/converter/language/fr-FR/upgrade/configuration.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']             = 'Edition des fichiers de configuration';
+
+// Text
+$_['text_curr_setting']	        = '(paramètre actuelle)';
+$_['text_images_info']          = 'Mise à jour des chemins d\'image';
+$_['text_intro_3']		= 'Cette mise à jour peut aussi être effectuée dans un mode <b>simulation</b>.<br />Activer cette option et aucune opération réel n\'est faite.';
+$_['text_intro_step_3']	        = 'Cela va renommer les répertoires <b>images/data</b> et mettre à jour les chemins d\'image dans la base de données';
+$_['text_upgrade_info']         = 'Mise à niveau de la configuration';
+$_['text_step_3_3']		= 'Etape 3/3';
+$_['text_update_config']        = 'Désormais vous pouvez aussi mettre à jour le fichier <b>config.php</b> si ce n\'est pas déjà fait';
+// Header
+$_['header_step_3']		= 'Ajuster les chemins d\'image';
+// msg
+$_['msg_config_added']  	= 'Le paramètre <b>%s</b> à été ajouté à <b>%s</b> avec succès';
+$_['msg_config_constant']	= 'Le paramètre <b>%s</b> à été ajouté à <b>%s</b> avec succès!';
+$_['msg_config_replace']        = 'Le chemin de fichier dans le fichier de configuration <b>%s</b> à été ajusté';
+$_['msg_config_skipped']        = 'L\'adoption des fichiers de configuration à été sauté';
+$_['msg_config_uptodate']	= 'Le fichier de configuration <b>%s</b> est déjà à jour';
+$_['msg_perm_file']		= 'Vous n\'avez pas la permission d\'écrire dans le fichier <b>%s</b>, pour procéder à la modification, changer la permission du fichier à 644 ou 666';
+?>
