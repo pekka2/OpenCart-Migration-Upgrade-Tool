1da1bef25d1f3e5f652a9b527578e745ff9ccec2
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:13:00 +0300

    Create permission.php

---
+++ upload/converter/language/nl-NL/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Toegang geweigerd!'; 
+
+// Text
+$_['button_permission']     = 'Administrator rechten';
+$_['text_permission']       = 'U bent niet gemachtigd om te toegang tot deze pagina, neem contact op met de beheerder.';
+?>
