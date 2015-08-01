35cff77b4eda3e68a82367cb18ce2c48cf92f5a1
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:53:05 +0300

    Create info.php

---
+++ upload/converter/language/fr-FR/upgrade/info.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Informations de mise à jour';
+// Text
+$_['text_database_info']            = 'Informations de votre base de données';
+$_['text_expired_tables']           = 'Total de tables expirées ou autres tables :';
+$_['text_missing_tables']           = 'Total de table manquantes :';
+$_['text_oc2_db_tables']            = 'Table de la base de données version 2.0 d\'OC :';
+$_['text_tables_complete']          = 'Toute les nouvelles tables sont dans votre base de données.';
+$_['text_upgrade_info']             = 'Informations de mise à jour';
+$_['text_your_database']            = 'Votre base de données :';
+$_['text_your_db_tables']           = 'Total de tables de votre base de données :';
+?>
