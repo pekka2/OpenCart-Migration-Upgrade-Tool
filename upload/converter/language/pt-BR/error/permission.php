9232b413da8208fe824b4ed31e6c14e770877679
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:19:52 +0300

    Create permission.php

---
+++ upload/converter/language/pt-BR/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Permissão Negada!'; 
+
+// Text
+$_['button_permission']     = 'Permissão administrativa';
+$_['text_permission']       = 'Você não tem permissão para acessar esta página, por favor, entre em contato com o administrador do sistema.';
+?>
