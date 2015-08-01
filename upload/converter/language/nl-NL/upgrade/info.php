407b84e71edb03ee5659f2a4b86cf48a8e890da8
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:14:50 +0300

    Create info.php

---
+++ upload/converter/language/nl-NL/upgrade/info.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']                 = 'Upgrade Informatie';
+// Text
+$_['text_home']                     = 'Home';
+$_['text_database_info']            = 'Ga naar database-informatie';
+$_['text_expired_tables']           = 'Verlopen of andere tabellen in totaal:';
+$_['text_missing_tables']           = 'Ontbrekende tabellen in totaal:';
+$_['text_oc2_db_tables']            = 'OC versie 2.0 databasetabellen:';
+$_['text_tables_complete']          = 'Alle nieuwe tabellen aanwezig in uw Database.';
+$_['text_upgrade_info']             = 'Upgrade Informatie';
+$_['text_your_database']            = 'Uw database:';
+$_['text_your_db_tables']           = 'Uw databasetabellen in totaal:';
+?>
