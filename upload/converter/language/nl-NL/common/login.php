151f59ba3f7cae67edbb117716541b968becdbb1
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:11:24 +0300

    Create login.php

---
+++ upload/converter/language/nl-NL/common/login.php
@@ -0,0 +1,19 @@
+<?php
+// header
+$_['heading_title']  = 'Opencart Migration & Upgrade Tool Dashboard';
+
+// Text
+$_['text_heading']   = 'Opencart Migration & Upgrade Tool Dashboard';
+$_['text_login']     = 'Voer uw login-gegevens in.';
+
+// Entry
+$_['entry_username'] = 'Gebruikersnaam:';
+$_['entry_password'] = 'Wachtwoord:';
+
+// Button
+$_['button_login']   = 'Login';
+
+// Error
+$_['error_login']    = 'Gebruikersnaam en/of wachtwoord onjuist.';
+$_['error_token']    = 'Ongeldige token in de sessie. Log opnieuw in.';
+?>
