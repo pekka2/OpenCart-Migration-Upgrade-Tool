18f04583d6c5ced1dadae3baca2a329954ccb7eb
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 15:59:12 +0300

    Create permission.php

---
+++ upload/converter/language/dk-DK/error/permission.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Tilladelse nægtet!'; 
+
+// Text
+$_['button_permission']     = 'Administrativ tilladelse';
+$_['text_permission']       = 'Du har ikke adgangsrettighed til denne side. Kontakt venligst systemadministratoren.';
+?>
