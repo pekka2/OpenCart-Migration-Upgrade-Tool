1973299efc13e332be9f53b68ebb32b7877d0ea6
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:06:42 +0300

    Create configuration.php

---
+++ upload/converter/language/he-HE/upgrade/configuration.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']             = 'עריכת קבצי תצורה';
+
+// Text
+$_['text_curr_setting']	        = '(הגדרה נוכחית)';
+$_['text_images_info']          = 'שדרוג של Imagepaths';
+$_['text_intro_3']		= 'This updater can be operated in a <b>simulation</b> mode also.<br />Enable this option and no real operations are made.';
+$_['text_intro_step_3']	        = 'This will rename <b>image/data</b> directories and update image paths in database';
+$_['text_upgrade_info']         = 'שדרוג תצורה';
+$_['text_step_3_3']		= 'שלב 3/3';
+$_['text_update_config']        = 'כעת באפשרותך גם לעדכן את הקבצים <b>config. php</b> אם טרם עשית זאת';
+// Header
+$_['header_step_3']		= 'Adjust image paths';
+// msg
+$_['msg_config_added']  	= 'Setting <b>%s</b> successfully added to <b>%s</b>';
+$_['msg_config_constant']	= 'Setting <b>%s</b>  successfully added to <b>%s</b>!';
+$_['msg_config_replace']        = 'Filepath in configurationfile <b>%s</b> adjusted';
+$_['msg_config_skipped']        = 'Adopting configuration files skipped';
+$_['msg_config_uptodate']	= 'Configurationfile <b>%s</b> already up-to-date';
+$_['msg_perm_file']		= 'No permission to write file <b>%s</b>, to proceed change first filepermission to 644 or 666';
+?>
