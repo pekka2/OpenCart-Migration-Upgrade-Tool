c2e7e4f210eed9063a6f90bf53d610e20956b789
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-01 16:09:07 +0300

    Create nl-NL.php

---
+++ upload/converter/language/nl-NL/nl-NL.php
@@ -0,0 +1,22 @@
+<?php
+// Locale
+$_['code']                          = 'nl-NL';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Home';
+
+// Button
+$_['button_permission']             = 'Administrator rechten';
+$_['button_save']                   = 'Opslaan';
+$_['button_upgrade']                = 'Start de update';
+
+// Btn
+$_['btn_skip']		= 'Overslaan';
+$_['btn_continue']	= 'Doorgaan';
+
+// Entry
+$_['entry_perms']		= 'Server schrijfrechten';
+// Help
+$_['help_perms']		= '(schrijven toestemming van map <b>system/logs</b>)';
+?>
