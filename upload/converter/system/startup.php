e9af27418a286caf2befc67c90498f82483118a2
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-08-03 17:48:52 +0300

    Update startup.php

--- upload/converter/system/startup.php
+++ upload/converter/system/startup.php
@@ -99,6 +99,5 @@ require_once(DIR_SYSTEM . 'library/log.php');
 require_once(DIR_SYSTEM . 'library/mail.php');
 require_once(DIR_SYSTEM . 'library/request.php');
 require_once(DIR_SYSTEM . 'library/response.php');
-require_once(DIR_SYSTEM . 'library/session.php');
 require_once(DIR_SYSTEM . 'library/template.php');
 ?>
