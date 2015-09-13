0a700c4abdc22fe30e51637fb336e7a06f461364
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-13 20:46:53 +0300

    Create clean.php

---
+++ upload/converter/language/fi-FI/upgrade/clean.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']          = 'Puhdista tietokanta';
+$_['heading_images']         = 'Kuvatiedostojen polkujen muutos';
+// Text
+$_['text_pause']		     = 'Tauko';
+$_['text_upload']		     = 'Et ole vielä siirtänyt Opencart version 2.0.x tiedostoja palvelimelle. Tässä vaiheessa on suositeltavaa, että ennen seuraavaa vaihetta siirrät version 2.0.x tiedostot palvelimelle sinun verkkokauppan';
+$_['text_upgrade_info']      = 'Päivitys Informaatio';
+$_['text_clean_info']        = 'Poista vanhentuneet asetukset';
+$_['text_clean_2_info']      = 'Poista vanhentuneet taulut ja sarakkeet';
+// btn
+$_['btn_back']  	         = 'Takaisin';
+$_['btn_preview']	         = 'Jatka esikatselutilassa';
+// header
+$_['header_step_configuration'] = 'Config.php tiedostoja on muokattu onnistuneesti';
+$_['header_step_images']        = 'Kuvapolut on vaihdettu ja kuvahakemistot on nimetty uudelleen onnistuneesti';
+$_['header_step_clean']         = 'Tietokanta on puhdistettu vanhasta rakenteesta onnistuneesti';
+// help
+$_['help_imageDir']		        = 'Määritä kansion nimi, jos se ei ole <b>image</i>';
+$_['help_ops']			        = 'Näytä päivityksen eteneminen <small>(näyttää kaikki tietokantaan tehdyt muutokset)</small>';
+$_['help_simulate']		        = 'Testi päivitys <small>(toiminnot ainoastaan testitilassa)</small>';
+// msg
+$_['msg_change_path']    	    = 'VAIHDETTU yhteensä <b>%s</b> kuvatiedosto(jen) polku(a) tietokannassa onnistuneesti';
+$_['msg_copy_image']	        = 'KOPIOITU yhteensä <b>%s</b> kuvatiedosto(a) onnistuneesti';
+$_['msg_errors']	            = 'Hakemistoa <b>%s</b> ei löydy! Tarkista polku.';
+$_['msg_image_copied']	        = 'Kuvatiedosto <b>%s/%s</b> kopioitu onnistuneesti';
+$_['msg_image_skipped']  	    = 'Kuvatiedostojen polkujen käsittely pyynnöstäsi ohitettu';
+$_['msg_perm_dir']		        = 'Ei kirjoitusoikeuksia hakemistoon <b>%s</b>. Muuta ensin käyttöoikeuksia (joko 0755 tai 0777)';
+$_['msg_renamed_dir']	        = 'Hakemisto <b>%s</b> nimeksi muutettu <b>%s</b> onnistuneesti';
+$_['msg_renamed_total_dir']     = 'Nimetty uudestaan yhteensä <b>%s</b> hakemisto(a) onnistuneesti';
+
+// error
+$_['error_image_not_found']	= 'Kuvahakemsitoa <b>%s</b> ei löydy! Tarkista hakemiston nimi.';
+?>
