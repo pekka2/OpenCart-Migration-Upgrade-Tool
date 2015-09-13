6a5ec7cfaf91a2789d3668aad05d7aa6fe7438f8
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-13 20:05:34 +0300

    Update en-US.php

--- upload/converter/language/en-US/en-US.php
+++ upload/converter/language/en-US/en-US.php
@@ -1,27 +1,43 @@
 <?php
 // Locale
-$_['code']                          = 'en-US';
-$_['direction']                     = 'ltr';
+$_['code']              = 'en-US';
+$_['direction']         = 'ltr';
 
 // Text
-$_['text_home']                     = 'Home';
+$_['text_home']         = 'Home';
 $_['text_off']			= 'OFF';
 $_['text_on']			= 'ON';
-$_['text_simulation']	        = 'Simulation is';
-
-// Button
-$_['button_database']               = 'To Upgrade The Database';
-$_['button_permission']             = 'Administrative permission';
-$_['button_save']                   = 'Save';
-$_['button_logout']                 = 'Logout';
-$_['button_upgrade']                = 'Initial Upgrade';
+$_['text_simulation']   = 'Simulation is';
+$_['text_step']		    = 'Step %s / %s';
 
 // Btn
-$_['btn_skip']		= 'Skip';
-$_['btn_continue']	= 'Continue';
+$_['btn_clean']	        = 'Clean settings';
+$_['btn_clean_dbase']   = 'Drop';
+$_['btn_collate']	    = 'Update collate';
+$_['btn_column']        = 'Add columns';
+$_['btn_continue']	    = 'Continue';
+$_['btn_config']		= 'Update config files';
+$_['btn_data']          = 'Add data';
+$_['btn_database']      = 'To Upgrade The Database';
+$_['btn_images']        = 'Upgrade image paths';
+$_['btn_info']          = 'Database information';
+$_['btn_logout']        = 'Logout';
+$_['btn_module']        = 'Move modules';
+$_['button_permission'] = 'Administrative permission';
+$_['btn_save']          = 'Save';
+$_['btn_skip']		    = 'Skip';
+$_['btn_start']			= 'Start upgrade';
+$_['btn_upgrade']       = 'Initial Upgrade';
 
-// Entry
-$_['entry_perms']		= 'Your server writing permission';
-// Help
-$_['help_perms']		= '(write permission of directory <b> system/logs </b>)';
+// Step
+$_['step_start']        = 'Add new tables';
+$_['step_collate']      = 'Database collation';
+$_['step_column']       = 'Add new columns';
+$_['step_data']         = 'Add new data';
+$_['step_module']       = 'Move modules';
+$_['step_setting']      = 'Add new settings';
+$_['step_configuration'] = 'Config.php files';
+$_['step_images']       = 'Kuvapolkujen vaihto';
+$_['step_clean_module'] = 'Clean settings';
+$_['step_clean_table']  = 'Drop expired structure';
 ?>
