da30b061ec3c7707ef44ba7e86f0b39412dd87ca
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-13 13:14:04 +0300

    Update structure.php

--- upload/converter/system/library/structure.php
+++ upload/converter/system/library/structure.php
@@ -28,6 +28,244 @@ class Structure {
           return $ret;	
   }
    
+   public function newData($upgrade){
+		$file = DIR_SQL . $upgrade . '.sql';
+
+		if (!file_exists($file)) {
+			exit('Could not load sql file: ' . $file);
+		}
+
+		$string = '';
+
+		$lines = file($file);
+
+		$status = false;
+
+		// Get only the create statements
+		foreach($lines as $line) {
+			// Set any prefix
+			$line = str_replace("CREATE TABLE IF NOT EXISTS `oc_", "CREATE TABLE IF NOT EXISTS `" . DB_PREFIX, $line);
+
+			// If line begins with create table we want to start recording
+			if (substr($line, 0, 12) == 'CREATE TABLE') {
+				$status = true;
+			}
+
+			if ($status) {
+				$string .= $line;
+			}
+
+			// If line contains with ; we want to stop recording
+			if (preg_match('/;/', $line)) {
+				$status = false;
+			}
+		}
+
+		$table_new_data = array();
+
+		// Trim any spaces
+		$string = trim($string);
+
+		// Trim any ;
+		$string = trim($string, ';');
+
+		// Start reading each create statement
+		$statements = explode(';', $string);
+
+		foreach ($statements as $sql) {
+			// Get all fields
+			$field_data = array();
+
+			preg_match_all('#`(\w[\w\d]*)`\s+((tinyint|smallint|mediumint|bigint|int|tinytext|text|mediumtext|longtext|tinyblob|blob|mediumblob|longblob|varchar|char|datetime|date|float|double|decimal|timestamp|time|year|enum|set|binary|varbinary)(\((.*)\))?){1}\s*(collate (\w+)\s*)?(unsigned\s*)?((NOT\s*NULL\s*)|(NULL\s*))?(auto_increment\s*)?(default \'([^\']*)\'\s*)?#i', $sql, $match);
+
+			foreach(array_keys($match[0]) as $key) {
+				$field_data[] = array(
+					'name'          => trim($match[1][$key]),
+					'type'          => strtoupper(trim($match[3][$key])),
+					'size'          => str_replace(array('(', ')'), '', trim($match[4][$key])),
+					'sizeext'       => trim($match[6][$key]),
+					'collation'     => trim($match[7][$key]),
+					'unsigned'      => trim($match[8][$key]),
+					'notnull'       => trim($match[9][$key]),
+					'autoincrement' => trim($match[12][$key]),
+					'default'       => trim($match[14][$key]),
+				);
+			}
+
+			// Get primary keys
+			$primary_data = array();
+
+			preg_match('#primary\s*key\s*\([^)]+\)#i', $sql, $match);
+
+			if (isset($match[0])) {
+				preg_match_all('#`(\w[\w\d]*)`#', $match[0], $match);
+			} else{
+				$match = array();
+			}
+
+			if ($match) {
+				foreach($match[1] as $primary) {
+					$primary_data[] = $primary;
+				}
+			}
+
+			// Get indexes
+			$index_data = array();
+
+			$indexes = array();
+
+			preg_match_all('#key\s*`\w[\w\d]*`\s*\(.*\)#i', $sql, $match);
+
+			foreach($match[0] as $key) {
+				preg_match_all('#`(\w[\w\d]*)`#', $key, $match);
+
+				$indexes[] = $match;
+			}
+
+			foreach($indexes as $index) {
+				$key = '';
+
+				foreach($index[1] as $field) {
+					if ($key == '') {
+						$key = $field;
+					} else{
+						$index_data[$key][] = $field;
+					}
+				}
+			}
+
+			// Table options
+			$option_data = array();
+
+			preg_match_all('#(\w+)=(\w+)#', $sql, $option);
+
+			foreach(array_keys($option[0]) as $key) {
+				$option_data[$option[1][$key]] = $option[2][$key];
+			}
+
+			// Get Table Name
+			preg_match_all('#create\s*table\s*if\s*not\s*exists\s*`(\w[\w\d]*)`#i', $sql, $table);
+
+			if (isset($table[1][0])) {
+				$table_new_data[] = array(
+					'sql'     => $sql,
+					'name'    => $table[1][0],
+					'field'   => $field_data,
+					'primary' => $primary_data,
+					'index'   => $index_data,
+					'option'  => $option_data
+				);
+			}
+		}
+		return $table_new_data;
+	}
+	public function oldData(){
+		$table_old_data = array();
+		$table_query = $this->db->query("SHOW TABLES FROM `" . DB_DATABASE . "`");
+		foreach ($table_query->rows as $table) {
+			if (utf8_substr($table['Tables_in_' . DB_DATABASE], 0, strlen(DB_PREFIX)) == DB_PREFIX || DB_PREFIX == '' ){
+				$field_data = array();
+
+				$field_query = $this->db->query("SHOW COLUMNS FROM `" . $table['Tables_in_' . DB_DATABASE] . "`");
+
+				foreach ($field_query->rows as $field) {
+					$field_data[] = $field['Field'];
+				}
+
+				$table_old_data[$table['Tables_in_' . DB_DATABASE]] = $field_data;
+			}
+		};
+         return $table_old_data;
+	}
+  public function language() {
+		 $sql = 'SELECT  * FROM  `' . DB_PREFIX . 'language`';
+
+          $languages = $this->db->query($sql);
+          
+          return $languages->rows;	
+  }
+  
+   public function hasLayout( $val ) {
+ 
+	$sql = '
+	SELECT
+		*
+	FROM
+		`' . DB_PREFIX . 'layout_module`
+	WHERE
+		`code` LIKE  \'' . $val . '%\'';
+       if( array_search( DB_PREFIX . 'layout_module' , $this->tables())){
+					$result = $this->db->query( $sql );
+					if( count( $result->row ) == 0 ) {
+						return false;
+					}
+     } else {
+				return false;
+      }
+	return true;
+   }
+
+   public function getProductOption() {
+ 
+	$sql = 'SELECT	*FROM `' . DB_PREFIX . 'product_option`';
+		$result = $this->db->query( $sql );
+		if( count( $result->rows ) >  0 ) {
+				return true;
+		}
+	 return false;
+   }
+   public function getLayout() {
+ 
+	$sql = 'SELECT * FROM `' . DB_PREFIX . 'layout`';
+       if( array_search( DB_PREFIX . 'layout' , $this->tables())){
+					$result = $this->db->query( $sql );
+					if( count( $result->row ) == 0 ) {
+						return false;
+					}
+     } else {
+				return false;
+      }
+	return true;
+   }
+   public function getReturnAction() {
+ 
+	$sql = 'SELECT	*  FROM	`' . DB_PREFIX . 'return_action`';
+       if( array_search( DB_PREFIX . 'return_action' , $this->tables())){
+					$result = $this->db->query( $sql );
+					if( count( $result->row ) == 0 ) {
+						return false;
+					}
+       } else {
+				return false;
+      }
+	return true;
+   }
+   public function getVoucherTheme() {
+ 
+	$sql = 'SELECT	*	FROM	`' . DB_PREFIX . 'voucher_theme`';
+       if( array_search( DB_PREFIX . 'voucher_theme' , $this->tables())){
+					$result = $this->db->query( $sql );
+					if( count( $result->row ) == 0 ) {
+						return false;
+					}
+     } else {
+				return false;
+      }
+	return true;
+   }
+   public function getCustomerGroupDescription() {
+ 
+	$sql = 'SELECT * FROM `' . DB_PREFIX . 'customer_group_description`';
+       if( array_search( DB_PREFIX . 'customer_group_description' , $this->tables())){
+					$result = $this->db->query( $sql );
+					if( count( $result->row ) == 0 ) {
+						return false;
+					}
+     } else {
+				return false;
+      }
+	return true;
+   }
   public function language() {
 		 $sql = '
                 SELECT 
