9b8f8e489bd8f6448d0a065d0db84c85c6039475
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 02:14:15 +0300

    Update configuration.php

--- upload/converter/language/fi-FI/upgrade/configuration.php
+++ upload/converter/language/fi-FI/upgrade/configuration.php
@@ -1,7 +1,7 @@
 <?php
 // Heading
-$_['heading_title']          = 'Muokkaa konfigurointi tiedostoja';
-$_['heading_images']         = 'Kuvatiedostojen polkujen muutos';
+$_['heading_title']           = 'Muokkaa konfigurointi tiedostoja';
+$_['heading_images']          = 'Kuvatiedostojen polkujen muutos';
 
 // Text
 $_['text_images_info']        = 'Päivitä kuvatiedostojen polut';
@@ -12,16 +12,16 @@ $_['text_update_config']      = 'Nyt voit myös päivittää  <b>config.php</b> 
 $_['header_step_images']      = 'Kuvapolut on vaihdettu ja kuvahakemistot on nimetty uudelleen onnistuneesti';
 $_['header_step_clean']       = 'Tietokanta on puhdistettu vanhasta rakenteesta onnistuneesti';
 // entry
-$_['entry_adminDir']	      = 'Admin tiedostojen hakemiston nimi';
+$_['entry_adminDir']       = 'Admin tiedostojen hakemiston nimi';
 // help
-$_['help_adminDir']		      = 'Määritä kansion nimi, jos se ei ole Admin';
+$_['help_adminDir']        = 'Määritä kansion nimi, jos se ei ole Admin';
 // msg
-$_['msg_config_added']  	  = 'Asetus <b>%s</b> on onnistuneesti Lisätty <b>%s</b> tauluun';
-$_['msg_config_constant']	  = 'Asetus <b>%s</b>  lisätty onnistuneesti tiedostoon <b>%s</b>!';
-$_['msg_config_replace']      = 'Asetustiedoston  <b>%s</b> polku määritelty';
-$_['msg_config_skipped']      = 'Asennustiedostot pyynnöstäsi ohitettu';
-$_['msg_config_uptodate']	  = 'Asetustiedosto <b>%s</b> on päivitetty';
-$_['msg_perm_file']		      = 'Ei oikeuksia kirjoittaa tiedostoon <b>%s</b>, muokkaa käyttöoikeudeksi joko 644 tai 666';
+$_['msg_config_added']     = 'Asetus <b>%s</b> on onnistuneesti Lisätty <b>%s</b> tauluun';
+$_['msg_config_constant']  = 'Asetus <b>%s</b>  lisätty onnistuneesti tiedostoon <b>%s</b>!';
+$_['msg_config_replace']   = 'Asetustiedoston  <b>%s</b> polku määritelty';
+$_['msg_config_skipped']   = 'Asennustiedostot pyynnöstäsi ohitettu';
+$_['msg_config_uptodate']  = 'Asetustiedosto <b>%s</b> on päivitetty';
+$_['msg_perm_file']        = 'Ei oikeuksia kirjoittaa tiedostoon <b>%s</b>, muokkaa käyttöoikeudeksi joko 644 tai 666';
 // error
-$_['error_image_not_found']	  = 'Kuvahakemsitoa <b>%s</b> ei löydy! Tarkista hakemiston nimi.';
+$_['error_image_not_found'] = 'Kuvahakemsitoa <b>%s</b> ei löydy! Tarkista hakemiston nimi.';
 ?>
