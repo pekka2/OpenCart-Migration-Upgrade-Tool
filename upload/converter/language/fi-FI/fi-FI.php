51372a85136b40c0c7a8adece9ddab25629ad7ca
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 02:08:18 +0300

    Update fi-FI.php

--- upload/converter/language/fi-FI/fi-FI.php
+++ upload/converter/language/fi-FI/fi-FI.php
@@ -1,43 +1,48 @@
 <?php
-
-$_['code']            = 'fi-FI';
-$_['direction']       = 'ltr';
+// Locale
+$_['code']              = 'fi-FI';
+$_['direction']         = 'ltr';
 
 // Text
 $_['text_home']         = 'Etusivu';
-$_['text_off']	        = 'POIS PÄÄLTÄ';
-$_['text_on']		= 'PÄÄLLÄ';
-$_['text_simulation']	= 'Testitila on';
-$_['text_step']	        = 'Vaihe %s / %s';
+$_['text_off']          = 'POIS PÄÄLTÄ';
+$_['text_on']           = 'PÄÄLLÄ';
+$_['text_simulation']   = 'Testitila on';
+$_['text_step']         = 'Vaihe %s / %s';
 
 // Btn
-$_['btn_clean']	        = 'Puhdista asetukset';
-$_['btn_clean_dbase']   = 'Poista vanhat sarakkeet';
+$_['btn_back']          = 'Takaisin';
+$_['btn_clean']         = 'Puhdista asetukset';
 $_['btn_collate']       = 'Vaihda aakkosjärjestys';
 $_['btn_column']        = 'Lisää sarakkeet';
-$_['btn_continue']	= 'Jatka';
+$_['btn_config']        = 'Päivitä asetus tiedostot';
+$_['btn_continue']      = 'Jatka';
 $_['btn_data']          = 'Lisää data';
 $_['btn_database']      = 'Päivitä tietokanta';
+$_['btn_drop']          = 'Poista vanhentunut rakenne';
+$_['btn_finish']        = 'Lopetus';
 $_['btn_images']        = 'Päivitä kuvapolut';
 $_['btn_info']          = 'Tietokannan tiedot';
+$_['btn_login']         = 'Kirjaudu';
 $_['btn_logout']        = 'Kirjaudu ulos';
 $_['btn_module']        = 'Siirrä moduulit';
 $_['btn_permission']    = 'Ylläpitäjän käyttöoikeudet';
+$_['btn_preview']       = 'Jatka esikatselutilassa';
 $_['btn_save']          = 'Tallenna';
 $_['btn_setting']       = 'Lisää asetukset';
-$_['btn_skip']		= 'Ohita';
-$_['btn_start']	        = 'Aloita päivitys';
+$_['btn_skip']          = 'Ohita';
+$_['btn_start']         = 'Aloita päivitys';
 $_['btn_upgrade']       = 'Päivityksen aloitus';
 
 // Step
-$_['step_start']        = 'Uudet taulut';
-$_['step_collate']      = 'Tietokannan aakkosjärjestys';
-$_['step_column']       = 'Uudet sarakkeet';
-$_['step_data']         = 'Uuden datan lisäys';
-$_['step_module']       = 'Moduulien siirto';
-$_['step_setting']      = 'Uudet asetukset';
-$_['step_configuration'] = 'Config.php tieostot';
-$_['step_images']       = 'Kuvapolkujen vaihto';
-$_['step_clean_module'] = 'Puhdista asetukset';
-$_['step_clean_table']  = 'Poista vanhat rakenteet';
+$_['step_start']         = 'Uudet taulut';
+$_['step_collate']       = 'Tietokannan aakkosjärjestys';
+$_['step_column']        = 'Uudet sarakkeet';
+$_['step_data']          = 'Uuden datan lisäys';
+$_['step_module']        = 'Moduulien siirto';
+$_['step_setting']       = 'Uudet asetukset';
+$_['step_configuration'] = 'Config.php tiedostot';
+$_['step_images']        = 'Kuvapolkujen vaihto';
+$_['step_clean_module']  = 'Puhdista asetukset';
+$_['step_clean_table']   = 'Poista vanhat rakenteet';
 ?>
