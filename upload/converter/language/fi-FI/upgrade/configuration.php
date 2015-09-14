d2a669b9ece604d425a1b0230353df6c756b683c
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 02:12:14 +0300

    Update configuration.php

--- upload/converter/language/fi-FI/upgrade/configuration.php
+++ upload/converter/language/fi-FI/upgrade/configuration.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']             = 'Muokkaa konfigurointi tiedostoja';
-$_['heading_database']         = 'Päivitä tietokanta';
+$_['heading_title']          = 'Muokkaa konfigurointi tiedostoja';
+$_['heading_images']         = 'Kuvatiedostojen polkujen muutos';
 
 // Text
-$_['text_curr_setting']	        = '(nykyinen asetus)';
-$_['text_images_info']          = 'Päivitä kuvatiedostojen polut';
-$_['text_intro_3']		= 'Tässä muuntajassa voit käyttää myös <b>simulointi</b> eli päivityksen esikatselutilaa.
-';
-$_['text_intro_step_3']	        = 'Tämä nimeää <b>image/data</b> hakemistot sekä vaihtaa kuvatiedostojen polut tietokannassa.';
-$_['text_upgrade_info']         = 'Kofigurointi asetusten päivitys';
-$_['text_step_3_3']		= 'Vaihe 3 / 3';
-$_['text_intro_step_3']	        = 'Tämä nimeää <b>image/data</b> hakemistot sekä vaihtaa kuvatiedostojen polut tietokannassa.';
-$_['text_configuration_info']   = 'Config.php tiedostojen päivitys';
-$_['text_update_config']        = 'Nyt voit myös päivittää  <b>config.php</b> tiedostoja, jos sitä ei ole vielä tehty';
+$_['text_images_info']        = 'Päivitä kuvatiedostojen polut';
+$_['text_intro_step_image']	  = 'Tämä nimeää <b>image/data</b> hakemistot sekä vaihtaa kuvatiedostojen polut tietokannassa.';
+$_['text_configuration_info'] = 'Config.php tiedostojen päivitys';
+$_['text_update_config']      = 'Nyt voit myös päivittää  <b>config.php</b> tiedostoja, jos sitä ei ole vielä tehty';
 // Header
-$_['header_step_3']		= 'Säädä kuvatiedostojen polut';
-$_['header_step_setting']  = 'Tietokantaan on lisätty uudet asetukset onnistuneesti';
+$_['header_step_images']      = 'Kuvapolut on vaihdettu ja kuvahakemistot on nimetty uudelleen onnistuneesti';
+$_['header_step_clean']       = 'Tietokanta on puhdistettu vanhasta rakenteesta onnistuneesti';
+// entry
+$_['entry_adminDir']	      = 'Admin tiedostojen hakemiston nimi';
+// help
+$_['help_adminDir']		      = 'Määritä kansion nimi, jos se ei ole Admin';
 // msg
-$_['msg_config_added']  	= 'Asetus <b>%s</b> on onnistuneesti Lisätty <b>%s</b> tauluun';
-$_['msg_config_constant']	= 'Asetus <b>%s</b>  lisätty onnistuneesti tiedostoon <b>%s</b>!';
-$_['msg_config_replace']        = 'Asetustiedoston  <b>%s</b> polku määritelty';
-$_['msg_config_skipped']        = 'Asennustiedostot pyynnöstäsi ohitettu';
-$_['msg_config_uptodate']	= 'Asetustiedosto <b>%s</b> on päivitetty';
-$_['msg_perm_file']		= 'Ei oikeuksia kirjoittaa tiedostoon <b>%s</b>, muokkaa käyttöoikeudeksi joko 644 tai 666';
+$_['msg_config_added']  	  = 'Asetus <b>%s</b> on onnistuneesti Lisätty <b>%s</b> tauluun';
+$_['msg_config_constant']	  = 'Asetus <b>%s</b>  lisätty onnistuneesti tiedostoon <b>%s</b>!';
+$_['msg_config_replace']      = 'Asetustiedoston  <b>%s</b> polku määritelty';
+$_['msg_config_skipped']      = 'Asennustiedostot pyynnöstäsi ohitettu';
+$_['msg_config_uptodate']	  = 'Asetustiedosto <b>%s</b> on päivitetty';
+$_['msg_perm_file']		      = 'Ei oikeuksia kirjoittaa tiedostoon <b>%s</b>, muokkaa käyttöoikeudeksi joko 644 tai 666';
+// error
+$_['error_image_not_found']	  = 'Kuvahakemsitoa <b>%s</b> ei löydy! Tarkista hakemiston nimi.';
 ?>
