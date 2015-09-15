3947a69b84647dc322cd4d2498cfc315b9006a99
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 15:45:25 +0300

    Update info.php

--- upload/converter/language/fi-FI/upgrade/info.php
+++ upload/converter/language/fi-FI/upgrade/info.php
@@ -10,4 +10,9 @@ $_['text_tables_complete']          = 'Kaikki version 2.0 taulut on sinun tietok
 $_['text_upgrade_info']             = 'Päivitys Info';
 $_['text_your_database']            = 'Sinun tietokanta:';
 $_['text_your_db_tables']           = 'Tauluja sinun tietokannassa yhteensä:';
+$_['text_your_oc_tables']           = 'Sinun Opencart version tauluja:';
+$_['text_your_other_tables']        = 'Muita tauluja (esim. laajennukset):';
+$_['text_your_version']             = 'Sinun kaupan Opencart versio:';
+$_['text_tables_to_version']        = 'Taulujen määrä valitsemassasi versiossa:';
+$_['text_to_version']               = 'Olet päivittämässä versioon:';
 ?>
