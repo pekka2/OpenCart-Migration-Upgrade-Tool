b26fbf66f06f2f61670a2e8fb0762ea6b4d553f2
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 15:44:29 +0300

    Update configuration.php

--- upload/converter/model/upgrade/configuration.php
+++ upload/converter/model/upgrade/configuration.php
@@ -2,15 +2,16 @@
 class ModelUpgradeConfiguration extends Model{
     private $lang;
     private $text;
+    private $dirAdmin;
+    private $sowOps;
+    private $upgrade;
    public function editConfig( $data ){
        $this->lang = $this->lmodel->get('upgrade_configuration');
-       $this->simulate = ( !empty( $data['simulate'] ) ? true : false );
-       $this->dirAdmin = ( !empty( $data['dirAdmin'] ) ? true : 'admin' ) .'/';
-       $this->dirImage =  ( !empty( $data['dirImage'] ) ? true : 'image' ) . '/';
-       $this->dirOld = ( !empty( $data['dirOld'] ) ? true : false );
-       $this->showOps  = ( !empty( $data['showOps'] ) ? true : false );
-       $this->upgrade  = $data['upgrade2030'];
-    
+       $this->simulate = ( !empty( $data['simulate'] ) ?  true:false );
+       $this->dirAdmin = ( !empty( $data['dirAdmin'] ) ?  'admin':'admin' ) .'/';
+       $this->showOps  = ( !empty( $data['showOps'] ) ? true:false );
+       $this->upgrade  = $data['upgrade'];
+
         $this->text = '';
 
 		$modification = 'define(\'DIR_MODIFICATION\', \'' . DIR_MODIFICATION . '\'); // OC 2';
@@ -21,88 +22,88 @@ class ModelUpgradeConfiguration extends Model{
                 array_pop($server);
                 $server = implode('/',$server);
                 
-		$http_server = 'define(\'HTTP_SERVER\', \'http://' . $server . '/\'); // OC 1.4+';
-		$https_server = 'define(\'HTTPS_SERVER\', \'https://' .$server . '/\'); // OC 1.4+';
+		$http_server = '//define(\'HTTP_SERVER\', \'http://' . $server . '/\'); // OC 1.4+';
+		$https_server = '//define(\'HTTPS_SERVER\', \'https://' .$server . '/\'); // OC 1.4+';
 		$https_catalog = 'define(\'HTTPS_CATALOG\', \'https://' .$server . '/\'); // OC 1.4+';
 		$db_port = 'define(\'DB_PORT\', \'3306\'); // OC 2.0.3 +';
 
 		// frontend
 	    $content = file_get_contents( DIR_DOCUMENT_ROOT . 'config.php' );
-            $check = $content;
+       $check = $content;
 
-		if( !strpos( $content, 'DIR_UPLOAD' ) || $this->upgrade > 2020 &&  !strpos( $content, 'DB_PORT' ) ) {
+		if( !strpos( $content, 'DIR_UPLOAD' ) || $this->upgrade > 2020  &&  !strpos( $content, 'DB_PORT') ) {
 
 		$this->text .= $this->msg('<p><hr/></p>');
           //   FILE yourstore.com/config.php
 			$fp = file( DIR_DOCUMENT_ROOT . 'config.php' );
-
-		   if( !strpos( $content, 'HTTP_SERVER' ) ) {
-			  array_splice( $fp, 1, 0, $http_server . "\r\n" );
-			  array_splice( $fp, 2, 0, $https_server . "\r\n" );
-		   }
-	   	  if( !strpos( $content, 'DIR_UPLOAD' ) ) {
-			array_splice( $fp, 18, 0, $modification . "\r\n" );
-			array_splice( $fp, 19, 0, $upload . "\r\n" );
-		   }
-		   if($this->upgrade2030 &&  !strpos( $content, 'DB_PORT' )) {
-			  array_splice( $fp, 20, 0, $db_port . "\r\n" );
-		   }
+          
+						  if( !strpos( $content, 'HTTP_SERVER' ) ) {
+							  array_splice( $fp, 1, 0, $http_server . "\r\n" );
+							  array_splice( $fp, 2, 0, $https_server . "\r\n" );
+						   }
+					   	if( !strpos( $content, 'DIR_UPLOAD' ) ) {
+								array_splice( $fp, 18, 0, $modification . "\r\n" );
+								array_splice( $fp, 19, 0, $upload . "\r\n" );
+						   }
+						    if($this->upgrade > 2020 &&  !strpos( $content, 'DB_PORT' )) {
+							  array_splice( $fp, 20, 0, $db_port . "\r\n" );
+						   }
 
 			$content = implode( '', $fp );
 
-			if( is_writable( DIR_DOCUMENT_ROOT . 'config.php' ) ) {
-				if( !$this->simulate ) {
-					$fw = fopen( DIR_DOCUMENT_ROOT . 'config.php', 'wb' );
-					fwrite( $fw, $content );
-					fclose( $fw );
-				}
-
-		               if( !strpos( $check, 'HTTP_SERVER' ) ) {
-						if( $this->showOps ){
-							$this->text .= '<p><pre>'.$http_server.'</pre></p>';
-						}
-			        	$this->text .= $this->msg( sprintf(  $this->lang['msg_config_added'],  'HTTP_SERVER', 'config.php' ) );
-						 if( $this->showOps ){
-						         $this->text .= '<p><pre>'.$https_server.'</pre></p>';
-						 }
-				        $this->text .= $this->msg( sprintf(  $this->lang['msg_config_added'],  'HTTPS_SERVER', 'config.php' ) );
+					if( is_writable( DIR_DOCUMENT_ROOT . 'config.php' ) ) {
+							if( !$this->simulate ) {
+								$fw = fopen( DIR_DOCUMENT_ROOT . 'config.php', 'wb' );
+								fwrite( $fw, $content );
+								fclose( $fw ); 
+							}
+							
+										    if( !strpos( $check, 'HTTP_SERVER' ) ) {
+								                if( $this->showOps ){
+												    	$this->text .= '<div class="notice"><pre>'.$http_server.'</pre></div>';
+								                }
+												$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'HTTP_SERVER', 'config.php' ) );
+								                if( $this->showOps ){
+												    	$this->text .= '<div class="notice"><pre>'.$https_server.'</pre></div>';
+								                }
+												$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'HTTPS_SERVER', 'config.php' ) );
+										   }
+	                if( $this->showOps ){
+					    	$this->text .= '<p><pre>'.$upload.'</pre></p>';
+	                }
+								$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'DIR_UPLOAD', 'config.php' ) );
+	                if( $this->showOps ){
+					    	$this->text .= '<p><pre>'.$modification.'</pre></p>';
+	                }
+							$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'DIR_MODIFICATION', 'config.php' ) );
+								
+					    if($this->upgrade > 2020 &&  !strpos( $content, 'DB_PORT' )) {		
+		                if( $this->showOps ){
+						    	$this->text .= '<p><pre>'.$db_port.'</pre></p>';
 		                }
-                                  if( $this->showOps ){
-				    	$this->text .= '<p><pre>'.$upload.'</pre></p>';
-                                  }
-				$text .= $this->msg( sprintf(  $this->lang['msg_config_added'],  'DIR_UPLOAD', 'config.php' ) );
-			          if( $this->showOps ){
-				    	$this->text .= '<p><pre>'.$modification.'</pre></p>';
-                                  }
-				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_added'],  'DIR_MODIFICATION', 'config.php' ) );	
-			    if($this->upgrade > 2020 &&  !strpos( $check, 'DB_PORT' )) {		
-                                 if( $this->showOps ){
-				    	$this->text .= '<p><pre>'.$db_port.'</pre></p>';
-                                 }
-				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DB_PORT', $file ) );
-                             }
-			}else{
-				$this->text .= $this->msg(  sprintf( $this->lang, 'msg_perm_file', 'config.php' ) );
-			}
-		}else{
+								$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DB_PORT', $file ) );
+		            }
+					}else{
+					//	$this->text .= $this->msg(  sprintf( $this->lang, 'msg_perm_file', 'config.php' ) );
+					}
+		} else {
 			$this->text .= $this->msg( sprintf(  $this->lang['msg_config_uptodate'], 'config.php', '' ) );
 		}
-
 		$this->text .= $this->msg('<p><hr/></p>');
-
-		// FILE yourstore.com/admin/config.php
+		
+		// FILE yourstore.com/admin/config.php		
 		$file		= $this->dirAdmin . 'config.php';
 		$content2	= file_get_contents( DIR_DOCUMENT_ROOT . $file );
 
 		if( !strpos( $content2, 'DIR_UPLOAD' ) ) {
 			$fp2 = file( DIR_DOCUMENT_ROOT . $file );
-		    if( !strpos( $content, 'HTTPS_CATALOG' ) ) {
-			  array_splice( $fp, 8, 0, $https_catalog . "\r\n" );
+		    if( !strpos( $content2, 'HTTPS_CATALOG' ) ) {
+			  array_splice( $fp2, 8, 0, $https_catalog . "\r\n" );
 		   }
 			array_splice( $fp2, 21, 0, $modification . "\r\n" );
 			array_splice( $fp2, 22, 0, $upload . "\r\n" );
-		    if( $this->upgrade > 2020 &&  !strpos( $content2, 'DB_PORT' )) {
-			  array_splice( $fp, 25, 0, $db_port . "\r\n" );
+		    if($this->upgrade > 2020 &&  !strpos( $content2, 'DB_PORT' )) {
+			  array_splice( $fp2, 25, 0, $db_port . "\r\n" );
 		   }
 
 			$string = implode( '', $fp2 );
@@ -111,29 +112,45 @@ class ModelUpgradeConfiguration extends Model{
 				if( !$this->simulate ) {
 					$fw = fopen( DIR_DOCUMENT_ROOT . $file, 'wb' );
 					fwrite( $fw, $string );
-					fclose( $fw );
+					fclose( $fw ); 
+                    if( !$this->structure->getUpgrade() ){
+				       // Upgrade Cache
+				       $memory = DIR_DATA . 'upgrade_cache.log';
+				       if(file_exists($memory)){
+				       	$cache = unserialize($memory);
+				        $cache['db'] = true;
+				        $cache['files'] = false;
+					  } else {
+				       	$cache = array('db' => true,'files' => false );
+					  }
+                       if($cache){
+					    $fw = fopen( $cache, 'wb' );
+					    fwrite( $fw, serialize($cache) );
+					    fclose( $fw );
+					   }
+			        }
 				}
 
-		    if( !strpos( $check, 'HTTPS_CATALOG' ) ) {
-                                if( $this->showOps ){
-				    	$this->text .= '<p><pre>'.$https_catalog.'</pre></p>';
-                                 }
-				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_added'],  'HTTPS_CATALOG', 'config.php' ) );
-		   }
-                                if( $this->showOps ){
+				    if( !strpos( $content2, 'HTTPS_CATALOG' ) ) {
+		                if( $this->showOps ){
+						    	$this->text .= '<p><pre>'.$https_catalog.'</pre></p>';
+		                }
+						$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'HTTPS_CATALOG', $file ) );
+				   }
+                if( $this->showOps ){
 				    	$this->text .= '<p><pre>'.$upload.'</pre></p>';
-                                }
-				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DIR_UPLOAD', $file ) );
-                                if( $this->showOps ){
+                }
+						$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DIR_UPLOAD', $file ) );
+                if( $this->showOps ){
 				    	$this->text .= '<p><pre>'.$modification.'</pre></p>';
-                                }
-				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DIR_MODIFICATION', $file ) );
-		        if($this->upgrade > 2020 &&  !strpos( $content2, 'DB_PORT' )) {		
-                               if( $this->showOps ){
+                }
+						$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DIR_MODIFICATION', $file ) );
+		    if($this->upgrade > 2020 &&  !strpos( $content2, 'DB_PORT' )) {		
+                if( $this->showOps ){
 				    	$this->text .= '<p><pre>'.$db_port.'</pre></p>';
-                                }
+                }
 						$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'], 'DB_PORT', $file ) );
-                       }
+            }
 
 			}else{
 				$this->text .= $this->msg(  sprintf( $this->lang['msg_perm_file'], 'config.php', $file ) );
@@ -144,7 +161,67 @@ class ModelUpgradeConfiguration extends Model{
 
 	return $this->text;
    }
-  public function msg( $data ){
-       return str_replace( $data, '<div class="msg round">' . $data .'</div>', $data);
+  public function constantFinishing($data){
+       $this->lang = $this->lmodel->get('upgrade_configuration');
+       $this->simulate = ( !empty( $data['simulate'] ) ?  true:false );
+       $this->dirAdmin = ( !empty( $data['dirAdmin'] ) ?  'admin':'admin' ) .'/';
+       $this->showOps  = ( !empty( $data['showOps'] ) ? true:false );
+
+        $this->text = '';
+
+		$http_server = '//define(\'HTTP_SERVER\', \'http://';
+		$http_server2 = 'define(\'HTTP_SERVER\', \'http://';
+		$https_server = '//define(\'HTTPS_SERVER\', \'https://';
+		$https_server2 = 'define(\'HTTPS_SERVER\', \'https://';
+
+		// frontend
+		$file = DIR_DOCUMENT_ROOT . 'config.php';
+	    $content = file_get_contents( $file );
+
+	    $content = str_replace($http_server,$http_server2,$content);
+	    $content = str_replace($https_server,$https_server2,$content);
+
+				if( !$this->simulate ) {
+					$fw = fopen( $file, 'wb' );
+					fwrite( $fw, $content );
+					fclose( $fw );
+
+					  // Upgrade Cache
+				       $memory = DIR_DATA . 'upgrade_cache.log';
+				       if(file_exists($memory)){
+				       	$cache = unserialize($memory);
+				        $cache['db'] = true;
+				        $cache['files'] = true;
+					  } else {
+				       	$cache = array('db' => true,'files' => true );
+					  }
+                       if($cache){
+					    $fw = fopen( $cache, 'wb' );
+					    fwrite( $fw, serialize($cache) );
+					    fclose( $fw );
+					   }
+				}
+
+                $server = $_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['SCRIPT_NAME']));
+                $server = explode('/',$server);
+                array_pop($server);
+                $server = implode('/',$server);
+		$http_server = 'define(\'HTTP_SERVER\', \'http://' . $server . '/\'); // OC 1.4+';
+		$https_server = 'define(\'HTTPS_SERVER\', \'https://' .$server . '/\'); // OC 1.4+';
+                if( $this->showOps ){
+				    	$this->text .= '<p><pre>'.$http_server.'</pre></p>';
+                }
+				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'HTTP_SERVER', 'config.php' ) );
+
+                if( $this->showOps ){
+				    	$this->text .= '<p><pre>'.$https_server.'</pre></p>';
+                }
+				$this->text .= $this->msg( sprintf(  $this->lang['msg_config_constant'],  'HTTPS_SERVER', 'config.php' ) );
+
+      return $this->text;
+
+  }
+  private function msg( $data ){
+       return str_replace( $data, '<div class="msg round">' . $data .'</div>'."\r\n", $data);
   }
 }
