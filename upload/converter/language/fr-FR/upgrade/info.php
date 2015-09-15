4868e800372ce9c712bbd8b857349bba59ab1b9f
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 15:36:52 +0300

    Update info.php

--- upload/converter/language/fr-FR/upgrade/info.php
+++ upload/converter/language/fr-FR/upgrade/info.php
@@ -8,6 +8,11 @@ $_['text_missing_tables']           = 'Total de table manquantes :';
 $_['text_oc2_db_tables']            = 'Table de la base de données version 2.0 d\'OC :';
 $_['text_tables_complete']          = 'Toute les nouvelles tables sont dans votre base de données.';
 $_['text_upgrade_info']             = 'Informations de mise à jour';
-$_['text_your_database']            = 'Votre base de données :';
+$_['text_your_database']            = 'Votre base de données';
 $_['text_your_db_tables']           = 'Total de tables de votre base de données :';
+$_['text_your_oc_tables']           = 'You OpenCart version database tables:';
+$_['text_your_other_tables']        = 'Additional tables (eg plug-ins.):';
+$_['text_your_version']             = 'Your trade OpenCart version:';
+$_['text_tables_to_version']        = 'The amount tables in your chosen version:';
+$_['text_to_version']               = 'You are upgrading to version:';
 ?>
