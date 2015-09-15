d8be766efe7c4ea844b2c2ca7a9d0e224ec79b1f
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-15 15:35:17 +0300

    Update info.php

--- upload/converter/language/dk-DK/upgrade/info.php
+++ upload/converter/language/dk-DK/upgrade/info.php
@@ -10,4 +10,9 @@ $_['text_tables_complete']          = 'Alle nye tabeller er i din Database.';
 $_['text_upgrade_info']             = 'Opgradere Info';
 $_['text_your_database']            = 'Din database:';
 $_['text_your_db_tables']           = 'Dine databasetabeller i alt:';
+$_['text_your_oc_tables']           = 'You OpenCart version database tables:';
+$_['text_your_other_tables']        = 'Additional tables (eg plug-ins.):';
+$_['text_your_version']             = 'Your trade OpenCart version:';
+$_['text_tables_to_version']        = 'The amount tables in your chosen version:';
+$_['text_to_version']               = 'You are upgrading to version:';
 ?>
