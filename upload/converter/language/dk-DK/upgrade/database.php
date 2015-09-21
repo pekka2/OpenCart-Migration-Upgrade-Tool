271803852b67af5fa4180c2c9d0fed4eab809df5
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-21 23:18:57 +0300

    Update database.php

--- upload/converter/language/dk-DK/upgrade/database.php
+++ upload/converter/language/dk-DK/upgrade/database.php
@@ -21,6 +21,8 @@ $_['entry_imageDir']		= 'I mappen billede';
 $_['entry_up_1564']		     = 'Upgrade database to version 1.5.6.4';
 $_['entry_up_201_202']		= 'Upgrade database to version 2.0.1 - 2.0.2.0';
 $_['entry_up_2030']		= 'Upgrade database to  version 2.0.3.1';
+$_['entry_migration_module'] = 'Move modules again (truncate first tables `layout_module` ja `module`)';
+$_['entry_up_2100']		     = 'Upgrade database to version 2.1.0.0';
 // Header
 $_['header_step_2']		= 'Database successfully updated to latest version';
 // help
