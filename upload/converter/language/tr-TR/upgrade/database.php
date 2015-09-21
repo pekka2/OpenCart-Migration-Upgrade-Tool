60d7e4bb746190767bf3e425442a7ecc70897865
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-21 23:13:34 +0300

    Update database.php

--- upload/converter/language/tr-TR/upgrade/database.php
+++ upload/converter/language/tr-TR/upgrade/database.php
@@ -26,6 +26,8 @@ $_['entry_imageDir']		= 'Resim dizini adı';
 $_['entry_up_1564']		     = 'Upgrade database to version 1.5.6.4';
 $_['entry_up_201_202']		= 'Veritabanını 2.0.1 - 2.0.2.0 sürümüne yükselt';
 $_['entry_up_2030']		= 'Veritabanını 2.0.3.1 sürümüne yükselt';
+$_['entry_migration_module'] = 'Move modules again (truncate first tables `layout_module` ja `module`)';
+$_['entry_up_2100']		     = 'Upgrade database to version 2.1.0.0';
 // Header
 $_['header_step_2']		= 'Veritabanı başarıyla en son sürümüne güncellendi';
 // help
