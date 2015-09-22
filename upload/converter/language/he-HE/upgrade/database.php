38c80799d559c2ccd2d1a7cfe4252faa56149a30
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 18:18:57 +0300

    Update database.php

--- upload/converter/language/he-HE/upgrade/database.php
+++ upload/converter/language/he-HE/upgrade/database.php
@@ -12,6 +12,7 @@ $_['text_skip_theme']           = 'דלג על ערכת הנושא';
 $_['text_module_info']      = 'Move modules to the new tables';
 $_['text_setting_info']     = 'Add new settings to database';
 $_['text_collate_info']     = 'Update to the next database collation';
+$_['text_column_info']      = 'Add new columns';
 $_['text_table_info']       = 'Start to add new tables';
 $_['text_update_config']        = 'Now you can also update the <b>config.php</b> files if not already done';
 $_['text_version']          = 'Your store database structure have been identified %s.';
