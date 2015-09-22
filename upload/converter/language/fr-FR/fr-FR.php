45fdb79988a425361439c3d15ac5945cb274a553
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 09:51:44 +0300

    Update fr-FR.php

--- upload/converter/language/fr-FR/fr-FR.php
+++ upload/converter/language/fr-FR/fr-FR.php
@@ -5,20 +5,19 @@ $_['direction']                     = 'ltr';
 
 // Text
 $_['text_home']                     = 'Accueil';
-$_['text_off']			    = 'OFF';
-$_['text_on']			    = 'ON';
+$_['text_off']			          = 'OFF';
+$_['text_on']			            = 'ON';
 $_['text_simulation']	        = 'La simulation est';
 
-// Button
-$_['button_database']               = 'Pour mettre à niveau la base de données';
-$_['button_permission']             = 'Autorisation d\'administration';
-$_['button_save']                   = 'Enregistrer';
-$_['button_logout']                 = 'Logout';
-$_['button_upgrade']                = 'Mise à niveau initiale';
-
 // Btn
-$_['btn_skip']		= 'Passer';
-$_['btn_continue']	= 'Continuer';
+$_['btn_continue']	             = 'Continuer';
+$_['btn_database']               = 'Pour mettre à niveau la base de données';
+$_['btn_logout']                 = 'Logout';
+$_['btn_permission']             = 'Autorisation d\'administration';
+$_['btn_save']                   = 'Enregistrer';
+$_['btn_skip']		               = 'Passer';
+$_['btn_upgrade']                = 'Mise à niveau initiale';
+
 
 // Entry
 $_['entry_perms']		= 'Votre autorisation d\'écriture sur le serveur';
