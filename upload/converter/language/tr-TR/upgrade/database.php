7351a2438605a6c7ff5c39240a494e3204d0254c
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:50:40 +0300

    Update database.php

--- upload/converter/language/tr-TR/upgrade/database.php
+++ upload/converter/language/tr-TR/upgrade/database.php
@@ -7,22 +7,12 @@ $_['text_exa_store_path']       = '<br><span class="script-filename">Yardım: Su
 $_['text_intro_1']		= 'Bu uygulama OpenCart 1.4.7-2.0.0.0 sürümlerinden sürüm 2.x e güncelleştirme için tasarlanmıştır';
 $_['text_intro_2']		= 'Aşağıdaki güncelleştirme işlemi 3 adımda yapılır: <ol><li>Veritabanı tabloları denetleme ve eksik tabloları ekleme, gerekli alanları dönüştürme</li> <li>Varolan yapılandırma dosyalarını dönüştürme (front &amp;  backend)</li><li>Resim yollarını dönüştürme</li></ol>';
 $_['text_intro_3']		= 'Bu Güncelleyici <b>simülasyon</b> modunda da işletilebilir. Bu seçeneği <br/> etkinleştirirseniz gerçek hiçbir işlem yapılmaz.';
-$_['text_off']			= 'KAPALI';
-$_['text_on']			= 'AÇIK';
-$_['text_simulation']	        = 'Simülasyon';
-$_['text_step_1_3']		= 'Adım 1/3';
-$_['text_step_2_3']		= 'Adım 2/3';
 $_['text_toggle_help']     	= 'Yardımı Göster/Sakla';
 $_['text_upgrade_info']         = 'Yükseltme Bilgisi';
 $_['text_update_theme']         = 'Temayı şimdi güncelleştir';
 $_['text_skip_theme']            = 'Temayı Atla';
 $_['text_update_config']        = 'Şimdi de <b>config. php</b> dosyaları güncelleyebilirsiniz eğer bitmemişse';
-// Btn
-$_['btn_config']		= 'Yapılandırma dosyalarını güncelleştir';
-$_['btn_start']			= 'Güncelleştirmeyi Başlat';
 // Entry
-$_['entry_adminDir']	        = 'Yönetim dosyaları için dizin adı';
-$_['entry_imageDir']		= 'Resim dizini adı';
 $_['entry_up_1564']		     = 'Upgrade database to version 1.5.6.4';
 $_['entry_up_201_202']		= 'Veritabanını 2.0.1 - 2.0.2.0 sürümüne yükselt';
 $_['entry_up_2030']		= 'Veritabanını 2.0.3.1 sürümüne yükselt';
@@ -31,7 +21,7 @@ $_['entry_up_2100']		     = 'Upgrade database to version 2.1.0.0';
 // Header
 $_['header_step_2']		= 'Veritabanı başarıyla en son sürümüne güncellendi';
 // help
-$_['help_adminDir']		= 'Klasör adını düzelt eğer yönetici değilse';$_['help_ops']			= 'İşlemleri Göster <small>(tüm veritabanı işlemleri görüntüleme)</small>';
+$_['help_ops']			= 'İşlemleri Göster <small>(tüm veritabanı işlemleri görüntüleme)</small>';
 $_['help_simulate']		= 'Simülasyon Dönüştürme <small>(yalnızca simülasyon işlemler)</small>';
 $_['help_usage']		= '<b>How to use this tool?</b><ol type="1"><li>If not already done, download the OpenCart v.2 package from <a href="http://www.opencart.com" target="_blank">OpenCart</a></li><li>Unzip that package locally</li><li>This script has to be placed in the subfolder <b>converter</b> folder of your shopinstallation (../converter))</li><li>Now you have two (2) options:<ol type="I"><li>Transfer all folders and files from the OpenCart v.2 package <b>into the installed shop</b></li><li><b>Create a new directory</b> and copy all folders and files from the OpenCart v.2 package into</li></ol></li><li>If you have chosen method II copy the folder <b>image</b> and the <b>2 config.php</b> from the old shop</li><li><b>Never use the installer from the OpenCart 2.x package!</b></li><li>Set your options above and click on <b>Continue</b></li><li>If you are finished with this upgrade, do not to forget to delete this script</li></ol>';
 // Msg
