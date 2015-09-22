34b5603ab256da1e42adeeeebb79e0d8ab66dd1f
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 13:59:32 +0300

    Update finish.php

--- upload/converter/language/pt-BR/upgrade/finish.php
+++ upload/converter/language/pt-BR/upgrade/finish.php
@@ -3,5 +3,5 @@
 $_['heading_title']           = 'Your Store upgrade is complete';
 $_['heading_clean_structure'] = 'Clean database';
 // header
-$_['header_step_complete']    = 'Database successfully updated to latest version';
+$_['header_step_complete']    = 'Banco de dados atualizado com êxito para a versão mais recente';
 ?>
