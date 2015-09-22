f4bd73e912d6c233d1579b4c2a8f27e4f076275f
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:12:55 +0300

    Update nl-NL.php

--- upload/converter/language/nl-NL/nl-NL.php
+++ upload/converter/language/nl-NL/nl-NL.php
@@ -7,12 +7,14 @@ $_['direction']                     = 'ltr';
 $_['text_home']                     = 'Home';
 
 // Btn
+$_['btn_config']		= 'Bijwerken van de configuratiebestanden';
 $_['btn_continue']	             = 'Doorgaan';
 $_['btn_database']               = 'To Upgrade The Database';
 $_['btn_logout']                 = 'Logout';
 $_['btn_permission']             = 'Administrator rechten';
 $_['btn_save']                   = 'Opslaan';
 $_['btn_skip']	                 = 'Overslaan';
+$_['btn_start']			= 'Beginnen met de update';
 $_['btn_upgrade']                = 'Start de update';
 
 // Entry
