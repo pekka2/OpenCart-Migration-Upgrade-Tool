7517d3e803c375bdcb2d423d2535bf74b52a0c97
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 13:04:07 +0300

    Update fr-FR.php

--- upload/converter/language/fr-FR/fr-FR.php
+++ upload/converter/language/fr-FR/fr-FR.php
@@ -11,6 +11,7 @@ $_['text_simulation']	        = 'La simulation est';
 $_['text_step']		           = 'Etape %s/%s';
 
 // Btn
+$_['btn_back']  	    = 'Back';
 $_['btn_clean']	        = 'Clean settings';
 $_['btn_clean_dbase']   = 'Drop';
 $_['btn_collate']       = 'Update collate';
