fd58de947baf1322df2762ad235c6995f8b6f630
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:59:00 +0300

    Create clean.php

---
+++ upload/converter/language/en-US/upgrade/clean.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']          = 'Clean database';
+$_['heading_config']         = 'Edit of Configuration Files';
+$_['heading_clean_setting']  = 'Database outdated structure dropping';
+// Text
+$_['text_clean']             = 'Clean database';
+$_['text_pause']		     = 'Pause';
+$_['text_upload']		     = 'Et ole vielä siirtänyt Opencart version 2.0.x tiedostoja palvelimelle. Tässä vaiheessa on suositeltavaa, että ennen seuraavaa vaihetta siirrät version 2.0.x tiedostot palvelimelle sinun verkkokauppan';
+$_['text_upgrade_info']      = 'Upgrade Information';
+$_['text_clean_info']        = 'Delete expired settings';
+$_['text_clean_structure']   = 'Drop expired tables and columns';
+// header
+$_['header_step_configuration'] = 'Config.php tiedostoja on muokattu onnistuneesti';
+$_['header_step_clean']         = 'Database is cleaned from expired settings succesfully';
+$_['header_step_clean2']        = 'Database outdated structure was dropped successfully';
+// help
+$_['help_config_1_4_x']      = 'Your OpenCart in version these a constant variables are the index.php file. Let you used  these constants, your need to return to this page to finalize the update shortly after the file transfer.';
+$_['help_ops']			     = 'Display Operations <small>(display all database operations)</small>';
+$_['help_simulate']		     = 'Simulate Converting <small>(simulating operations only)</small>';
+// error
+$_['error_admin_not_found']	= 'Managed by the directory <b>%s </ b> not  found! Check the directory name.';
+?>
