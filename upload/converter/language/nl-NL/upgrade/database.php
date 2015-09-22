4ffc77748bdc896aa38b7a61035ace8b68580d36
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 18:18:40 +0300

    Update database.php

--- upload/converter/language/nl-NL/upgrade/database.php
+++ upload/converter/language/nl-NL/upgrade/database.php
@@ -14,6 +14,7 @@ $_['text_skip_theme']            = 'Overslaan thema';
 $_['text_module_info']      = 'Move modules to the new tables';
 $_['text_setting_info']     = 'Add new settings to database';
 $_['text_collate_info']     = 'Update to the next database collation';
+$_['text_column_info']      = 'Add new columns';
 $_['text_table_info']       = 'Start to add new tables';
 $_['text_update_config']        = 'Nu kunt u ook de <b>config. php</b> bestanden bijwerken indien dat nog niet is gedaan';
 // Entry
