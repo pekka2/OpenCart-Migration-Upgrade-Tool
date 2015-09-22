5038a0d59401b1ca655b7eb29ee5a9d3dbf21e1a
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:02:30 +0300

    Update dk-DK.php

--- upload/converter/language/dk-DK/dk-DK.php
+++ upload/converter/language/dk-DK/dk-DK.php
@@ -10,6 +10,7 @@ $_['text_on']			    = 'TIL';
 $_['text_simulation']	        = 'Simulation er';
 
 // Btn
+$_['btn_config']		= 'Opdater config filer';
 $_['btn_continue']	= 'Fortsat';
 $_['btn_database']               = 'At opgradere databasen';
 $_['btn_login']                  = 'Login';
@@ -17,7 +18,8 @@ $_['btn_logout']                 = 'Logout';
 $_['btn_permission']             = 'Administrativ tilladelse';
 $_['btn_save']                   = 'Gem';
 $_['btn_skip']		= 'Springe';
-$_['btn_upgrade']                = 'Indledende opgradering';
+$_['btn_upgrade']                = 'Indlede
+$_['btn_start']			= 'Starte med opdateringen';nde opgradering';
 
 
 // Entry
