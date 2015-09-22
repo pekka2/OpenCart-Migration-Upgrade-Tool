0a84f7bc31ccbe613943cc03cd578e562e8771d3
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 18:27:34 +0300

    Update database.php

--- upload/converter/language/dk-DK/upgrade/database.php
+++ upload/converter/language/dk-DK/upgrade/database.php
@@ -13,6 +13,7 @@ $_['text_module_info']      = 'Move modules to the new tables';
 $_['text_setting_info']     = 'Add new settings to database';
 $_['text_collate_info']     = 'Update to the next database collation';
 $_['text_column_info']      = 'Add new columns';
+$_['text_data_info']        = 'Add the data to the new database columns';
 $_['text_table_info']       = 'Start to add new tables';
 $_['text_update_config']        = 'Now you can also update the <b>config.php</b> files if not already done';
 $_['text_version']          = 'Your store database structure have been identified %s.';
