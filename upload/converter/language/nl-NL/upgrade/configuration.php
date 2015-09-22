080b2697f41872e48a171e64a4273a5bcde49071
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 13:00:41 +0300

    Update configuration.php

--- upload/converter/language/nl-NL/upgrade/configuration.php
+++ upload/converter/language/nl-NL/upgrade/configuration.php
@@ -8,11 +8,7 @@ $_['text_curr_setting']	        = '(huidige instelling)';
 $_['text_images_info']          = 'Upgrade van Imagepaths';
 $_['text_intro_3']		= 'Deze updater kan ook bediend worden in een <b>simulatie</b> mode .<br /> Schakel deze optie inschakelen in en er zullen geen echte bewerkingen worden gemaakt.';
 $_['text_intro_step_3']	        = 'Dit zal de map <b>image/data</b> hernoemen en de image-map paden aanpassen';
-$_['text_off']			= 'UIT';
-$_['text_on']			= 'AAN';
 $_['text_upgrade_info']         = 'Upgrade van de configuratie';
-$_['text_simulation']	        = 'Simulatie is';
-$_['text_step_3_3']		= 'Stap 3/3';
 $_['text_update_config']        = 'Nu kunt u ook de <b>config. php</b> bestanden bijwerken indien dat nog niet is gedaan';
 // Entry
 $_['entry_adminDir']	        = 'De naam van de map voor adminfiles';
