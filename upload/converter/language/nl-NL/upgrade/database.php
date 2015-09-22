7742638d1f0bd13a195ddace7bae578d5cb78cf8
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 14:02:03 +0300

    Update database.php

--- upload/converter/language/nl-NL/upgrade/database.php
+++ upload/converter/language/nl-NL/upgrade/database.php
@@ -2,36 +2,22 @@
 // Heading
 $_['heading_title']             = 'Upgrade van Database';
 // Text
-$_['text_home']                     = 'Home';
 $_['text_exa_store_path']       = '<br><span class="script-filename">Help: The path to the file to your server is: <em>%s</em>.</span>';
 $_['text_intro_1']		= 'Dit programma is bedoeld voor OpenCart versies 1.4.7-2.0.0.0 voor het bijwerken naar versie 2.x';
 $_['text_intro_2']		= 'The following update process is made in 3 steps:<ol><li>Check database tables and add missing tables, convert required fields</li><li>Convert existing configuration files (front- &amp; backend)</li><li>Convert image paths</li></ol>';
 $_['text_intro_3']		= 'Deze updater kan ook bediend worden in een <b>simulatie</b> mode .<br /> Schakel deze optie inschakelen in en er zullen geen echte bewerkingen worden gemaakt.';
-$_['text_off']			= 'UIT';
-$_['text_on']			= 'AAN';
-$_['text_simulation']	        = 'Simulatie is';
-$_['text_step_1_3']		= 'Stap 1/3';
-$_['text_step_2_3']		= 'Stap 2/3';
 $_['text_toggle_help']     	= 'Toon/Verberg Help';
 $_['text_upgrade_info']         = 'Upgrade Informatie';
 $_['text_update_theme']         = 'Update thema nu';
 $_['text_skip_theme']            = 'Overslaan thema';
 $_['text_update_config']        = 'Nu kunt u ook de <b>config. php</b> bestanden bijwerken indien dat nog niet is gedaan';
-// Btn
-$_['btn_config']		= 'Bijwerken van de configuratiebestanden';
-$_['btn_start']			= 'Beginnen met de update';
 // Entry
-$_['entry_adminDir']	        = 'De naam van de map voor adminfiles';
-$_['entry_imageDir']		= 'In de naam van de image map';
 $_['entry_up_1564']		     = 'Upgrade database to version 1.5.6.4';
 $_['entry_up_201_202']		= 'De database een upgrade uitvoert naar versie 2.0.1-2.0.2.0';
 $_['entry_up_2030']		= 'Upgrade van de database naar versie 2.0.3.1';
 $_['entry_migration_module'] = 'Move modules again (truncate first tables `layout_module` ja `module`)';
 $_['entry_up_2100']		     = 'Upgrade database to version 2.1.0.0';
-// Header
-$_['header_step_2']		= 'Database is succesvol bijgewerkt naar de nieuwste versie';
 // help
-$_['help_adminDir']		= 'Aanpassen van de mapnaam als niet admin';
 $_['help_ops']			= 'Weergeven operaties <small>(weergeven alle databasebewerkingen)</small>';
 $_['help_simulate']		= 'Simuleren omzetten <small>(simuleren operaties alleen)</small>';
 $_['help_usage']		= '<b>How to use this tool?</b><ol type="1"><li>If not already done, download the OpenCart v.2 package from <a href="http://www.opencart.com" target="_blank">OpenCart</a></li><li>Unzip that package locally</li><li>This script has to be placed in the subfolder <b>converter</b> folder of your shopinstallation (../converter))</li><li>Now you have two (2) options:<ol type="I"><li>Transfer all folders and files from the OpenCart v.2 package <b>into the installed shop</b></li><li><b>Create a new directory</b> and copy all folders and files from the OpenCart v.2 package into</li></ol></li><li>If you have chosen method II copy the folder <b>image</b> and the <b>2 config.php</b> from the old shop</li><li><b>Never use the installer from the OpenCart 2.x package!</b></li><li>Set your options above and click on <b>Continue</b></li><li>If you are finished with this upgrade, do not to forget to delete this script</li></ol>';
