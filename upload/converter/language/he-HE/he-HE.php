c291a62cbcc0a88d22a10ea496507730b7978ee1
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 13:13:19 +0300

    Update he-HE.php

--- upload/converter/language/he-HE/he-HE.php
+++ upload/converter/language/he-HE/he-HE.php
@@ -27,6 +27,7 @@ $_['btn_info']          = 'Database information';
 $_['btn_logout']                 = 'Logout';
 $_['btn_module']        = 'Move modules';
 $_['btn_permission']             = 'הרשאה מנהלית';
+$_['btn_preview']	    = 'Continue to preview status';
 $_['btn_save']                   = 'שמור';
 $_['btn_skip']	              	 = 'דלג';
 $_['btn_start']			             = 'התחל בעידכון';
