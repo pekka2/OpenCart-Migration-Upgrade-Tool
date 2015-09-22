4fef87b0e45c1fa759002310fbb2dd6324d21904
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:51:40 +0300

    Update database.php

--- upload/converter/language/pt-BR/upgrade/database.php
+++ upload/converter/language/pt-BR/upgrade/database.php
@@ -2,27 +2,17 @@
 // Heading
 $_['heading_title']             = 'Atualização do banco de dados';
 // Text
-$_['text_home']                     = 'Home';
 $_['text_exa_store_path']       = '<br><span class="script-filename">Ajuda: O caminho para o arquivo no seu servidor é: <em>%s</em>.</span>';
 $_['text_intro_1']		= 'Este programa destina-se para OpenCart versões 1.4.7-2.0.0.0 para atualizar para a versão 2.x';
 $_['text_intro_2']		= 'O processo de atualização a seguir é feito em 3 etapas: <ol><li>Conferir as tabelas do banco de dados e adicionar as tabelas que faltam, converter os campos obrigatórios</li><li>Converter os arquivos de configuração existentes (front-end &amp; backend)</li><li>Converter caminhos das imagens</li></ol>';
 $_['text_intro_3']		= 'Esta atualização pode ser executada em um modo de <b>simulação</b> também.<br />Habilite esta opção e nenhuma alteração será efetuada.';
-$_['text_off']			= 'Desligado';
-$_['text_on']			= 'Ligado';
-$_['text_simulation']	        = 'Simulador está';
-$_['text_step_1_3']		= 'Passo 1 / 3';
-$_['text_step_2_3']		= 'Passo 2 / 3';
 $_['text_toggle_help']     	= 'Exibir/Esconder Ajuda';
 $_['text_upgrade_info']         = 'Info de atualização';
 $_['text_update_theme']         = 'Atualizar tema agora';
 $_['text_skip_theme']            = 'Ignorar tema';
 $_['text_update_config']        = 'Agora você pode atualizar os arquivos <b>config.php</b> se ainda não tiverem sido feitos';
-// Btn
-$_['btn_config']		= 'Atualizar os arquivos de configuração';
-$_['btn_start']			= 'Comece com a atualização';
+
 // Entry
-$_['entry_adminDir']	        = 'O nome do diretório para arquivos do Admin';
-$_['entry_imageDir']		= 'Nome do diretório de imagens';
 $_['entry_up_1564']		     = 'Upgrade database to version 1.5.6.4';
 $_['entry_up_201_202']		= 'Atualizar o banco de dados para versão 2.0.1-2.0.2.0';
 $_['entry_up_2030']		= 'Atualizar o banco de dados para a versão 2.0.3.1';
@@ -31,7 +21,7 @@ $_['entry_up_2100']		     = 'Upgrade database to version 2.1.0.0';
 // Header
 $_['header_step_2']		= 'Banco de dados atualizado com êxito para a versão mais recente';
 // help
-$_['help_adminDir']		= 'Ajustar o nome da pasta, se não for admin';$_['help_ops']			= 'Exibir as operações <small>(Exibir todas as operações do banco de dados)</small>';
+$_['help_ops']			= 'Exibir as operações <small>(Exibir todas as operações do banco de dados)</small>';
 $_['help_simulate']		= 'Simular a conversão <small>(simulando operações apenas)</small>';
 $_['help_usage']		= '<b>Como usar este módulo?</b><ol type="1"><li>Se ainda não tiver feito, baixe o OpenCart v.2 de <a href="http://www.opencart.com" target="_blank">OpenCart</a></li><li>Descompacte o pacote localmente</li><li>Este script precisa ser colocado numa subpasta chamada <b>converter</b> dentro da raíz da loja (../converter)</li><li>Agora você tem 2 opções:<ol type="I">Transferir todas as pastas e arquivos do OpenCart v.2 <b> para dentro da loja já instalada</b></li><li><b>Crie uma nova pasta</b> e copie todas as pastas e arquivos do OpenCart v.2 para dentro</li></ol></li><li>Se você tiver escolhido o método II copie a pasta <b>image</b> e os <b>2 config.php</b> da antiga loja</li><li><b>Nunca use o instalador do OpenCart 2.x!</b></li><li>Defina as opções acima e clique em <b>Continuar</b></li><li>Se você já tiver finalizado esta atualização, não esqueça de deletar este script</li></ol>';
 // Msg
