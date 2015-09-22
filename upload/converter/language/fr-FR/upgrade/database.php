48fbe8f037a465c9065d2bbef785bf49232a9c62
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 18:27:15 +0300

    Update database.php

--- upload/converter/language/fr-FR/upgrade/database.php
+++ upload/converter/language/fr-FR/upgrade/database.php
@@ -13,6 +13,7 @@ $_['text_module_info']      = 'Move modules to the new tables';
 $_['text_setting_info']     = 'Add new settings to database';
 $_['text_collate_info']     = 'Update to the next database collation';
 $_['text_column_info']      = 'Add new columns';
+$_['text_data_info']        = 'Add the data to the new database columns';
 $_['text_table_info']       = 'Start to add new tables';
 $_['text_version']          = 'Your store database structure have been identified %s.';
 $_['text_update_config']        = 'Désormais vous pouvez aussi mettre à jour le fichier <b>config.php</b> si ce n\'est pas déjà fait';
