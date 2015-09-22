4e095b37a5ce3dcc1c4f6bf4513979cf3bb29f4e
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 13:03:23 +0300

    Update de-DE.php

--- upload/converter/language/de-DE/de-DE.php
+++ upload/converter/language/de-DE/de-DE.php
@@ -11,6 +11,7 @@ $_['text_simulation']	        = 'Simulation ist';
 $_['text_step']	          	  = 'Schritt %s / %s';
 
 // Btn
+$_['btn_back']  	    = 'Back';
 $_['btn_clean']	        = 'Clean settings';
 $_['btn_clean_dbase']   = 'Drop';
 $_['btn_collate']       = 'Update collate';
