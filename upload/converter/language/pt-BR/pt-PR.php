022f842b235b53d49e59a909af330e3541860142
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 10:14:12 +0300

    Update pt-PR.php

--- upload/converter/language/pt-BR/pt-PR.php
+++ upload/converter/language/pt-BR/pt-PR.php
@@ -7,11 +7,14 @@ $_['direction']                     = 'ltr';
 $_['text_home']                     = 'Home';
 
 // Btn
+$_['btn_config']		= 'Atualizar os arquivos de configuração';
+$_['btn_start']			= 'Comece com a atualização';
 $_['btn_continue']	            = 'Continuar';
 $_['btn_database']               = 'Para atualizar o banco de dados';
 $_['btn_logout']                 = 'Logout';
 $_['btn_permission']             = 'Permissão administrativa';
 $_['btn_save']                   = 'Salvar';
+$_['btn_start']			= 'Comece com a atualização';
 $_['btn_skip']		               = 'Ignorar';
 $_['btn_upgrade']                = 'Iniciar atualização';
 
