c8e515072ae8ec22c25db47eee5b454c20e83f7f
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-22 11:36:02 +0300

    Update finish.php

--- upload/converter/language/fi-FI/upgrade/finish.php
+++ upload/converter/language/fi-FI/upgrade/finish.php
@@ -2,4 +2,6 @@
 // Heading
 $_['heading_title']           = 'Kaupan päivitys on valmis';
 $_['heading_clean_structure'] = 'Tietokannnan vanhentuneen rakenteen poisto';
+// header
+$_['header_step_complete']   = 'Tietokanta on päivitetty onnistuneesti valitsemaasi versioon';
 ?>
