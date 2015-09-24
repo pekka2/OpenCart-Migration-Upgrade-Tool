3f8ba110dbe1886c1b059edfa29e78717e0b5e4e
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-24 15:10:57 +0300

    Update rename_columns.sql

--- SQL/rename_columns.sql
+++ SQL/rename_columns.sql
@@ -1,8 +1,6 @@
 # RENAME NEW COLUMS
 ALTER TABLE `oc_product_option` CHANGE `option_value` `value` text NOT NULL;
 ALTER TABLE `oc_setting` CHANGE `group` `code` varchar(32) NOT NULL;
-ALTER TABLE `oc_order` CHANGE `invoice_id` `invoice_no` int(11) NOT NULL;
-ALTER TABLE `oc_order` CHANGE `currency` `currency_code` varchar(3) NOT NULL;
 ALTER TABLE `oc_order_recurring` CHANGE `profile_reference` `reference` varchar(255) NOT NULL;
 ALTER TABLE `oc_order_recurring` CHANGE `profile_name` `recurring_name` varchar(255) NOT NULL;
 ALTER TABLE `oc_order_recurring` CHANGE `profile_description` `recurring_description` varchar(255) NOT NULL;
