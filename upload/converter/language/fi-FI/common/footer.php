a4756687e10771b1d5f4523b5169fcae22b8cb91
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-09-30 15:48:25 +0300

    Update footer.php

--- upload/converter/language/fi-FI/common/footer.php
+++ upload/converter/language/fi-FI/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 // Text
 $_['text_to_my_store']	= 'Go to my store';
-$_['text_footer']       = 'OpenCart Converter &copy; ' . date('Y') . ' versio %s';
+$_['text_footer']       = 'OpenCart Converter &copy;2015 versio %s';
 ?>
