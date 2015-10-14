26bd5f6ec974bd05f336926e0dbe2a04a7b995b8
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-10-14 22:51:18 +0300

    Create logout.php

---
+++ upload-2.04/converter/controller/common/logout.php
@@ -0,0 +1,10 @@
+<?php       
+class ControllerCommonLogout extends Controller {   
+	public function index() { 
+	   
+			 setcookie("UpgradeMigration", HTTP_SERVER, time() - 3600);
+
+		$this->redirect($this->url->link('common/login'));
+	}
+}  
+?>
