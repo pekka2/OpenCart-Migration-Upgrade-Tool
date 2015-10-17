a9aa8f2bed451cccfe30a453617bf6a66a13d5a1
Pekka Mansikka <pekka2@users.noreply.github.com>
2015-10-17 11:18:24 +0300

    Create en-US.php

---
+++ upload-2.04/converter/language/en-US/en-US.php
@@ -0,0 +1,27 @@
+<?php
+// Locale
+$_['code']                          = 'en-US';
+$_['direction']                     = 'ltr';
+
+// Text
+$_['text_home']                     = 'Home';
+$_['text_off']			    = 'OFF';
+$_['text_on']			    = 'ON';
+$_['text_simulation']	        = 'Simulation is';
+
+// Button
+$_['button_database']               = 'To Upgrade The Database';
+$_['button_permission']             = 'Administrative permission';
+$_['button_save']                   = 'Save';
+$_['button_logout']                 = 'Logout';
+$_['button_upgrade']                = 'Initial Upgrade';
+
+// Btn
+$_['btn_skip']		= 'Skip';
+$_['btn_continue']	= 'Continue';
+
+// Entry
+$_['entry_perms']		= 'Your server writing permission';
+// Help
+$_['help_perms']		= '(write permission of directory <b> system/logs </b>)';
+?>
