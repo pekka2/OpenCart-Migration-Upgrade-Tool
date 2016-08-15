2a1e0dc494fe668a56e51feddda05ca01e06cf33
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 12:46:29 +0300

    Delete permission.php

--- upload/converter/language/nl-NL/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Toegang geweigerd!'; 
-
-// Text
-$_['button_permission']     = 'Administrator rechten';
-$_['text_permission']       = 'U bent niet gemachtigd om te toegang tot deze pagina, neem contact op met de beheerder.';
-?>
