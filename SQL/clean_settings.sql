ceb4bd5c535381c40c40360b9978a660586c203b
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:11:15 +0300

    Delete clean_settings.sql

--- SQL/clean_settings.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `oc_setting` WHERE `key` like '%module%';
-DELETE FROM `oc_setting` WHERE `key` like '%openbay%';
-DELETE FROM `oc_setting` WHERE `group` like '%bank_transfer%';
-DELETE FROM `oc_extension` WHERE `type` = 'payment' AND `code` = 'bank_transfer';
