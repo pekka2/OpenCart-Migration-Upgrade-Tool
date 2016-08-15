01f78c515b0eceb1645b45901181ed8dcc2db9cd
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:32:25 +0300

    Delete finish.php

--- upload/converter/language/fi-FI/upgrade/finish.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title']           = 'Kaupan päivitys on valmis';
-$_['heading_clean_structure'] = 'Tietokannnan vanhentuneen rakenteen poisto';
-// header
-$_['header_step_complete']   = 'Tietokanta on päivitetty onnistuneesti valitsemaasi versioon';
-?>
