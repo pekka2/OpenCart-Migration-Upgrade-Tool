c1b15e90dbf0d1cc605b3c294ce6628dcfb4121a
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:21:19 +0300

    Delete logout.php

--- upload/converter/controller/common/logout.php
+++
@@ -1,9 +0,0 @@
-<?php       
-class ControllerCommonLogout extends Controller {   
-	public function index() { 
-		setcookie("UpgradeMigration", HTTP_SERVER, time() - 3600);
-
-		$this->redirect($this->url->link('common/login');
-	}
-}  
-?>
