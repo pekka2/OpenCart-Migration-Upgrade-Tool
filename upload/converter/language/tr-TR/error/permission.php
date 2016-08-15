d2a124ccf23f164c50ed648fc0db3c27e8cb412b
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 12:49:19 +0300

    Delete permission.php

--- upload/converter/language/tr-TR/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'İzin Engellendi!'; 
-
-// Text
-$_['button_permission']     = 'Yönetim İzni';
-$_['text_permission']       = 'Bu sayfaya erişim izniniz yok, lütfen sistem yöneticinize başvurun.';
-?>
