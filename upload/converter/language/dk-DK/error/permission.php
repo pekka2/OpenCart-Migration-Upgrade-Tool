a0089b464c87eea957e3246e2394fd9a12a2f823
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:26:07 +0300

    Delete permission.php

--- upload/converter/language/dk-DK/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Tilladelse nægtet!'; 
-
-// Text
-$_['button_permission']     = 'Administrativ tilladelse';
-$_['text_permission']       = 'Du har ikke adgangsrettighed til denne side. Kontakt venligst systemadministratoren.';
-?>
