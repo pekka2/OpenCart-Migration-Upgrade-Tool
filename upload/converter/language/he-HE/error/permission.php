c3f9dc6f210788877cfdc5de500136e54f5473fe
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 12:45:06 +0300

    Delete permission.php

--- upload/converter/language/he-HE/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'הרשאה נדחיתה!'; 
-
-// Text
-$_['button_permission']     = 'הרשאה מנהלית';
-$_['text_permission']       = 'אין לך הרשאה לגשת לדף זה, אנא פנה למנהל המערכת שלך.';
-?>
