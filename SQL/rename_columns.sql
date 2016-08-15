b19e2e90763bb2588f6dcb52f1a2bbb0bec49506
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:11:45 +0300

    Delete rename_columns.sql

--- SQL/rename_columns.sql
+++
@@ -1,9 +0,0 @@
-# RENAME NEW COLUMS
-ALTER TABLE `oc_product_option` CHANGE `option_value` `value` text NOT NULL;
-ALTER TABLE `oc_setting` CHANGE `group` `code` varchar(32) NOT NULL;
-ALTER TABLE `oc_order_recurring` CHANGE `profile_reference` `reference` varchar(255) NOT NULL;
-ALTER TABLE `oc_order_recurring` CHANGE `profile_name` `recurring_name` varchar(255) NOT NULL;
-ALTER TABLE `oc_order_recurring` CHANGE `profile_description` `recurring_description` varchar(255) NOT NULL;
-ALTER TABLE `oc_order_recurring` CHANGE `profile_id` `recurring_id` int(11) NOT NULL;
-ALTER TABLE `oc_order_recurring` CHANGE `created` `date_added` datetime NOT NULL;
-ALTER TABLE `oc_order_recurring_transaction` CHANGE `created` `date_added` datetime NOT NULL;
