340c89f2c668f7d5c135aaa3d30b404237df302f
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:29:53 +0300

    Delete permission.php

--- upload/converter/language/en-US/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Permission Denied!'; 
-
-// Text
-$_['button_permission']     = 'Administrative permission';
-$_['text_permission']       = 'You do not have permission to access this page, please refer to your system administrator.';
-?>
