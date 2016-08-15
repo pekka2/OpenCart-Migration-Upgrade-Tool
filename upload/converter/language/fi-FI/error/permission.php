01a7b2b37aa10d60afa6c1aeb505d8566f0b25b7
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:31:54 +0300

    Delete permission.php

--- upload/converter/language/fi-FI/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Ei oikeuksia!'; 
-
-// Text
-$_['button_permission']     = 'Ylläpitäjän käyttöoikeudet';
-$_['text_permission']       = 'Sinulla ei ole oikeuksia nähdä tätä sivua, ota tarvittaessa yhteys järjestelmän ylläpitäjään.';
-?>
