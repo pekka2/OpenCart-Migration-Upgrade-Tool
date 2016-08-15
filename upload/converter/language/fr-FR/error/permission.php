2863720f459c8c00968e02266a50d7a7e65a8623
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 12:43:42 +0300

    Delete permission.php

--- upload/converter/language/fr-FR/error/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Autorisation refusée !'; 
-
-// Text
-$_['button_permission']     = 'Autorisation d\'administration';
-$_['text_permission']       = 'Vous n\'êtes pas autorisé à accéder à cette page, veuillez vous référer à votre administrateur système.';
-?>
