c4faaaf70abfd23e9e08150d1141ed31121f44dd
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 08:26:01 +0300

    Delete not_found.php

--- upload/converter/language/dk-DK/error/not_found.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Siden blev ikke fundet!';
-
-// Text
-$_['text_not_found'] = 'Siden du søger kan ikke findes! Kontakt administratoren, hvis problemet fortsætter.';
-?>
