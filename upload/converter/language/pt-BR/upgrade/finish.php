56a9cc3105c302f727f4f154a791aa6822b8a85f
Pekka Mansikka <pekka2@users.noreply.github.com>
2016-08-15 12:48:26 +0300

    Delete finish.php

--- upload/converter/language/pt-BR/upgrade/finish.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title']           = 'Your Store upgrade is complete';
-$_['heading_clean_structure'] = 'Clean database';
-// header
-$_['header_step_complete']    = 'Banco de dados atualizado com êxito para a versão mais recente';
-?>
