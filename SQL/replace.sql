67cf8caac0e56460e0433d7b813707d11249c693
Pekka Mansikka <pekka2@users.noreply.github.com>
2017-08-02 17:15:33 +0300

    Update replace.sql

--- SQL/replace.sql
+++ SQL/replace.sql
@@ -3,5 +3,6 @@ UPDATE `oc_category` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
 UPDATE `oc_manufacturer` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
 UPDATE `oc_product` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
 UPDATE `oc_product_image` SET `image` =   REPLACE ( image , 'data/', 'catalog/' );
+UPDATE `oc_product_description` SET `description` = REPLACE (description , 'data/', 'catalog/');
 UPDATE `oc_setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_logo';
 UPDATE `oc_setting` SET `value` =  REPLACE ( value , 'data/', 'catalog/' ) WHERE `key` = 'config_icon';
